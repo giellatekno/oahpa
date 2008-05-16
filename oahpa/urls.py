7c3c4971be33aea9f0887c11a400c7ca8ea1e69b
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2008-05-16 09:01:04 +0000

    Added url-definitions for drills.

--- oahpa/urls.py
+++ oahpa/urls.py
@@ -3,6 +3,15 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^oahpa/', include('oahpa.foo.urls')),
+    (r'^i18n/', include('django.conf.urls.i18n')),
+#    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/saara/ped'}),
+#    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^mgame/$', 'ped.drill.views.index'),
+    (r'^quizz/$', 'ped.drill.views.quizz'),
+    (r'^num/$', 'ped.drill.views.numgame'),
+    (r'^qa/$', 'ped.drill.views.qagame'),
+    (r'^bare/$', 'ped.drill.views.bare'),
+    (r'^context/$', 'ped.drill.views.context'),                       
 
     # Uncomment this for admin:
 #     (r'^admin/', include('django.contrib.admin.urls')),
