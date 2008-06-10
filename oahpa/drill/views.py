27f203c5b273a6f9ce6710ed4c1671b74b658d7f
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2008-06-10 11:52:35 +0000

    created function for pos-specific games.

--- oahpa/drill/views.py
+++ oahpa/drill/views.py
@@ -12,109 +12,150 @@ from qagame import *
 class Info:
     pass
 
-def mgame(request):
-    
-    count=0
-    correct=0
+class Gameview:
 
-    show_data=0
-    settings = Info()
-    settings.syll = []
-    settings.partofsp="N"
-    settings.case="N-ILL"
-    settings.books=[]
-    settings.semtype="NATURE"
-    settings.allbooks=[]
-    settings.language="sme"
-    settings.gametype="bare"
+    def init_settings(self):
 
-    if request.method == 'POST':
-        data = request.POST.copy()
+        show_data=0
+        self.settings=Info()
+        self.settings.syll = []
+        self.settings.pos="N"
+        self.settings.case="N-ILL"
+        self.settings.mood="Ind"
+        self.settings.tense="Prs"
+        self.settings.books=[]
+        self.settings.semtype="NATURE"
+        self.settings.allbooks=[]
+        self.settings.language="sme"
+        self.settings.gametype="bare"
+        self.settings.vtype_bare="PRS"
+        self.settings.vtype="VERB"
 
-        # Settings form is checked and handled.
-        settings_form = MorphForm(request.POST)
-        
+    def syll_settings(self,settings_form):
+
         if 'bisyllabic' in settings_form.data:
-            settings.syll.append('bisyllabic')
+            self.settings.syll.append('bisyllabic')
         if 'trisyllabic' in settings_form.data:
-            settings.syll.append('trisyllabic')
+            self.settings.syll.append('trisyllabic')
         if 'contracted' in settings_form.data:
-            settings.syll.append('contracted')
-        if len(settings.syll) == 0:
-            settings.syll.append('bisyllabic')
+            self.settings.syll.append('contracted')
+        if len(self.settings.syll) == 0:
+            self.settings.syll.append('bisyllabic')        
 
-        if settings_form.data['pos']:
-            settings.partofsp= settings_form.data['pos']
+    def create_mgame(self,request):
 
-        if settings_form.data['case']:
-            settings.case= settings_form.data['case']
+        count=0
+        correct=0
 
-        settings.books = settings_form.data['book']
-
-        if settings_form.allbooks:
-            settings.allbooks=settings_form.allbooks
-
-        if settings_form.data['gametype']:
-            settings.gametype= settings_form.data['gametype']
-
-        # Create game
-        if settings.gametype == "bare":
-            game = BareGame(settings)
-        else:
-            print "OK"
-            game = QAGame(settings)
-            game.init_tags()
+        if request.method == 'POST':
+            data = request.POST.copy()
             
-        # If settings are changed, a new game is created
-        # Otherwise the game is created using the user input.
-        if "settings" in data:
+            # Settings form is checked and handled.
+            settings_form = MorphForm(request.POST)
+            
+            self.syll_settings(settings_form)
+            
+            if settings_form.data.has_key('case'):
+                self.settings.case= settings_form.data['case']
+
+            if settings_form.data.has_key('vtype'):
+                self.settings.vtype= settings_form.data['vtype']
+
+            if settings_form.data.has_key('vtype_bare'):
+                self.settings.vtype_bare= settings_form.data['vtype_bare']
+
+            self.settings.books = settings_form.data['book']
+                
+            if settings_form.allbooks:
+                self.settings.allbooks=settings_form.allbooks
+                
+            if settings_form.data['gametype']:
+                self.settings.gametype= settings_form.data['gametype']
+                        
+            # Create game
+            if self.settings.gametype == "bare":
+                game = BareGame(self.settings)
+            else:
+                game = QAGame(self.settings)
+                game.init_tags()
+            
+            # If settings are changed, a new game is created
+            # Otherwise the game is created using the user input.
+            if "settings" in data:
+                game.new_game()
+            else:
+                game.check_game(data)
+                game.get_score(data)
+
+            if 'test' in data:
+                game.count=1
+            if "show_correct" in data:
+                show_correct = 1
+
+        # If there is no POST data, default settings are applied
+        else:
+            settings_form = MorphForm()
+            self.settings.syll.append('bisyllabic')
+            self.settings.books="all"
+
+            if settings_form.allbooks:
+                self.settings.allbooks=settings_form.allbooks
+            
+            game = BareGame(self.settings)
             game.new_game()
-        else:
-            game.check_game(data)
-            game.get_score(data)
 
-        if 'test' in data:
-            game.count=1
-        if "show_correct" in data:
-            show_correct = 1
+        c = Context({
+            'settingsform': settings_form,
+            'syllabic': self.settings.syll,
+            'forms': game.form_list,
+            'count': game.count,
+            'gametype': self.settings.gametype,
+            'score': game.score,
+            'case': self.settings.case,
+            'all_correct': game.all_correct,
+            'show_correct': game.show_correct,
+            })
+        return c
 
-    # If there is no POST data, default settings are applied
-    else:
-        settings_form = MorphForm()
-        settings.syll.append('bisyllabic')
-        settings.books="all"
+def mgame_n(request):
 
-        if settings_form.allbooks:
-            settings.allbooks=settings_form.allbooks
+    mgame = Gameview()
+    mgame.init_settings()
+    mgame.settings.pos = "N"
 
-            
-        game = BareGame(settings)
-        game.new_game()
+    c = mgame.create_mgame(request)
+    return render_to_response('mgame_n.html', c)
 
-    c = Context({
-        'settingsform': settings_form,
-        'syllabic': settings.syll,
-        'forms': game.form_list,
-        'count': game.count,
-        'gametype': settings.gametype,
-        'score': game.score,
-        'case': settings.case,
-        'all_correct': game.all_correct,
-        'show_correct': game.show_correct,
-    })
-    return render_to_response('mgame.html', c)
+
+def mgame_v(request):
+
+    mgame = Gameview()
+    mgame.init_settings()
+    mgame.settings.pos = "V"
+
+    c = mgame.create_mgame(request)
+    return render_to_response('mgame_v.html', c)
+
+def mgame_a(request):
+
+    mgame = Gameview()
+    mgame.init_settings()
+    mgame.settings.pos = "A"
+
+    c = mgame.create_mgame(request)
+    return render_to_response('mgame_a.html', c)
 
 
             
 def quizz(request):
 
-    settings = Info()
-    settings.semtype="NATURE"
-    settings.transtype="nobsme"
-    settings.books="all"
-    settings.allbooks=[]
-    settings.allsem=[]
-    settings.language="sme"
+    mgame = Gameview()
+    mgame.init_settings()
+    
+    mgame.settings.transtype="nobsme"
+    mgame.settings.books="all"
+    mgame.settings.allbooks=[]
+    mgame.settings.allsem=[]
 
     if request.method == 'POST':
         data = request.POST.copy()
@@ -122,17 +163,17 @@ def quizz(request):
         # Settings form is checked and handled.
         settings_form = QuizzForm(request.POST)
         
-        settings.semtype = settings_form.data['semtype']
-        settings.transtype = settings_form.data['transtype']
-        settings.books = settings_form.data['book']
+        mgame.settings.semtype = settings_form.data['semtype']
+        mgame.settings.transtype = settings_form.data['transtype']
+        mgame.settings.books = settings_form.data['book']
 
         if settings_form.allbooks:
-            settings.allbooks=settings_form.allbooks
+            mgame.settings.allbooks=settings_form.allbooks
         if settings_form.allsem:
-            settings.allsem=settings_form.allsem
+            mgame.settings.allsem=settings_form.allsem
 
 
-        game = QuizzGame(settings)
+        game = QuizzGame(mgame.settings)
 
         if "settings" in data:
             game.new_game()
@@ -150,10 +191,10 @@ def quizz(request):
     else:
         settings_form = QuizzForm()
         if settings_form.allbooks:
-            settings.allbooks=settings_form.allbooks
+            mgame.settings.allbooks=settings_form.allbooks
         if settings_form.allsem:
-            settings.allsem=settings_form.allsem
-        game = QuizzGame(settings)
+            mgame.settings.allsem=settings_form.allsem
+        game = QuizzGame(mgame.settings)
         game.new_game()
 
 
@@ -171,12 +212,14 @@ def quizz(request):
 
 def numgame(request):
 
-    settings = Info()
-    settings.maxnum = 10
-    settings.numgame = "numeral"
-    settings.books = []
-    settings.semtype=""
-    settings.language="sme"
+    mgame = Gameview()
+    mgame.init_settings()
+
+    mgame.settings.maxnum = 10
+    mgame.settings.numgame = "numeral"
+    mgame.settings.books = []
+    mgame.settings.semtype=""
+    mgame.settings.language="sme"
 
     if request.method == 'POST':
         data = request.POST.copy()
@@ -184,11 +227,11 @@ def numgame(request):
         # Settings form is checked and handled.
         settings_form = NumForm(request.POST)
         
-        settings.maxnum = settings_form.data['maxnum']
-        settings.numgame = settings_form.data['numgame']
-        settings.language = settings_form.data['language']
+        mgame.settings.maxnum = settings_form.data['maxnum']
+        mgame.settings.numgame = settings_form.data['numgame']
+        mgame.settings.language = settings_form.data['language']
 
-        game = NumGame(settings)
+        game = NumGame(mgame.settings)
 
         if "settings" in data:
             game.new_game()
@@ -205,7 +248,7 @@ def numgame(request):
     # If there is no POST data, default settings are applied
     else:
         settings_form = NumForm()
-        game = NumGame(settings)
+        game = NumGame(mgame.settings)
         game.new_game()
 
 
@@ -220,51 +263,4 @@ def numgame(request):
 
     return render_to_response('num.html', c)
 
-
-def qagame(request):
-
-    settings = Info()
-
-    if request.method == 'POST':
-        data = request.POST.copy()
-
-        # Settings form is checked and handled.
-        settings_form = MorphForm(request.POST)
-        
-        settings.pos = settings_form.data['maxnum']
-        settings.numgame = settings_form.data['numgame']
-
-        game = QAGame(settings)
-        game.init_tags()
-
-        if "settings" in data:
-            game.new_game()
-        else:
-            game.check_game(data)
-            game.get_score(data)
-
-        if 'test' in data:
-            game.count=1
-        if "show_correct" in data:
-            game.show_correct = 1
-
-        
-    # If there is no POST data, default settings are applied
-    else:
-        settings_form = QAForm()
-        game = QAGame(settings)
-        game.init_tags()
-        game.new_game()
-
-
-    c = Context({
-        'settingsform': settings_form,
-        'forms': game.form_list,
-        'count': game.count,
-        'score': game.score,
-        'all_correct': game.all_correct,
-        'show_correct': game.show_correct,
-    })
-
-    return render_to_response('qa.html', c)
 
