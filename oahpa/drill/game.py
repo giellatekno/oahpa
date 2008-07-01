0db25aac2bb3fcd3641b461a0b463ed4be4dc17b
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2008-07-01 10:01:22 +0000

    QA implemented.

--- oahpa/drill/game.py
+++ oahpa/drill/game.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from django.template import Context, loader
 from oahpa.drill.models import *
 from oahpa.drill.forms import *
@@ -44,10 +43,10 @@ class Game:
         
     def new_game(self):
         self.form_list = []
-        for n in range (0, self.num_fields):
-            db_info = Info()
-            db_info.userans = ""
-            db_info.correct = ""
+        for n in range (1, self.num_fields):
+            db_info = {}
+            db_info['userans'] = ""
+            db_info['correct'] = ""
             self.get_db_info(db_info)
             self.create_form(db_info, n, 0)
 
@@ -55,49 +54,53 @@ class Game:
             # No questions found, so the quiz_id must have been bad.
             raise Http404('Invalid quiz id.')
 
+    def search_info(self, reObj, string, value, words, type):
+
+        matchObj=reObj.search(string) 
+        if matchObj:
+            syntax = matchObj.expand(r'\g<syntaxString>')
+            if not words.has_key(syntax):
+                words[syntax] = {}
+            if not words[syntax].has_key(type):
+                words[syntax][type] = []
+            words[syntax][type].append(value)
+ 
+            print syntax, value
+
+        return words
+
     def check_game(self, data=None):
 
-        db_info = Info()
+        db_info = {}
+
+        question_tagObj=re.compile(r'^question_tag_(?P<syntaxString>[\w\-]*)$', re.U)
+        question_wordObj=re.compile(r'^question_word_(?P<syntaxString>[\w\-]*)$', re.U)
+        question_fullformObj=re.compile(r'^question_fullform_(?P<syntaxString>[\w\-]*)$', re.U)
+        answer_tagObj=re.compile(r'^answer_tag_(?P<syntaxString>[\w\-]*)$', re.U)
+        answer_wordObj=re.compile(r'^answer_word_(?P<syntaxString>[\w\-]*)$', re.U)
+        answer_fullformObj=re.compile(r'^answer_fullform_(?P<syntaxString>[\w\-]*)$', re.U)
 
         # If POST data was data check, regenerate the form using ids.
-        for n in range (0, self.num_fields):
-            if n == 0:
-                n_word_id = "word_id"
-                n_tag_id = "tag_id"
-                n_question_id = "question_id"
-                n_numeral_id = "numeral_id"
-                n_qstring = "qstring"
-                n_astring = "astring"
-                n_userans = "userans"
-                n_correct = "correct"
-            else:
-                n_word_id = str(n) + "-word_id"
-                n_tag_id = str(n) + "-tag_id"
-                n_question_id = str(n) + "-question_id"
-                n_numeral_id = str(n) + "-numeral_id"
-                n_qstring = str(n) + "-qstring"
-                n_astring = str(n) + "-astring"
-                n_userans = str(n) + "-userans"
-                n_correct = str(n) + "-correct"
+        for n in range (1, self.num_fields):
+            qwords = {}
+            awords = {}
+            selected_awords = {}
+            for d, value in data.items():
+                if d.count(str(n) + '-')>0:
+                    d = d.lstrip(str(n) + '-')
+                    qwords = self.search_info(question_tagObj, d, value, qwords, 'tag')
+                    qwords = self.search_info(question_wordObj, d, value, qwords, 'word')
+                    qwords = self.search_info(question_fullformObj, d, value, qwords, 'fullform')
+                    awords = self.search_info(answer_tagObj, d, value, selected_awords, 'tag')
+                    awords = self.search_info(answer_wordObj, d, value, selected_awords, 'word')
+                    awords = self.search_info(answer_fullformObj, d, value, selected_awords, 'fullform')
+                    
+                    db_info['qwords'] = qwords
+                    db_info['awords'] = awords
+                    db_info[d] = value
 
-            if "word_id" in data:
-                db_info.word_id=data[n_word_id]
-            if "question_id" in data:
-                db_info.question_id=data[n_question_id]
-            if "tag_id" in data:
-                db_info.tag_id=data[n_tag_id]
-            if "numeral_id" in data:
-                db_info.numeral=data[n_numeral_id]
-
-            if "qstring" in data:
-                db_info.qstring=data[n_qstring]
-            if "astring" in data:
-                db_info.astring=data[n_astring]
-                
-            db_info.userans = data[n_userans]
-            db_info.correct = data[n_correct]
-
-            self.create_form(db_info, n, data)
+            new_db_info = self.get_db_info(db_info)
+            self.create_form(new_db_info, n, data)
             
     def get_score(self, data):
 
@@ -148,16 +151,16 @@ class ContextGame(Game):
             
             form_count = Form.objects.filter(Q(word__pk=word_id) & Q(tag__pk=tag_id)).count()
             if word_id and form_count>0:
-                db_info.word_id = word_id
-                db_info.tag_id = tag_id
-                db_info.question_id = question_id
+                db_info['word_id'] = word_id
+                db_info['tag_id'] = tag_id
+                db_info['question_id'] = question_id
                 return
 
     def create_form(self, db_info, n, data=None):
 
-        word_id = db_info.word_id
-        tag_id = db_info.tag_id
-        question_id = db_info.question_id
+        word_id = db_info['word_id']
+        tag_id = db_info['tag_id']
+        question_id = db_info['question_id']
 
         form_list=Form.objects.filter(Q(word__pk=word_id) & Q(tag__pk=tag_id))
         if not form_list:
@@ -168,7 +171,7 @@ class ContextGame(Game):
         question_list = Question.objects.filter(Q(id=question_id))
         tr_list=Translationnob.objects.filter(Q(word__pk=word_id))
         
-        morph = (MorphQuestion(word, tag, form_list, tr_list, question_list[0], db_info.userans, db_info.correct, data, prefix=n))
+        morph = (MorphQuestion(word, tag, form_list, tr_list, question_list[0], db_info['userans'], db_info['correct'], data, prefix=n))
         self.form_list.append(morph)
 
 
@@ -217,15 +220,15 @@ class BareGame(Game):
                 
             form_count = Form.objects.filter(Q(word__pk=word_id) & Q(tag__pk=tag_id)).count()
             if word_id and form_count>0:
-                db_info.word_id = word_id
-                db_info.tag_id = tag_id
+                db_info['word_id'] = word_id
+                db_info['tag_id'] = tag_id
                 return
 
 
     def create_form(self, db_info, n, data=None):
 
-        word_id = db_info.word_id
-        tag_id = db_info.tag_id
+        word_id = db_info['word_id']
+        tag_id = db_info['tag_id']
 
         form_list=Form.objects.filter(Q(word__pk=word_id) & Q(tag__pk=tag_id))
         if not form_list:
@@ -234,7 +237,7 @@ class BareGame(Game):
         word = Word.objects.get(Q(id=word_id))
         tag = Tag.objects.get(Q(id=tag_id))
         tr_list=Translationnob.objects.filter(Q(word__pk=word_id))
-        morph = (MorphQuestion(word, tag, form_list, tr_list, "", db_info.userans, db_info.correct, data, prefix=n))
+        morph = (MorphQuestion(word, tag, form_list, tr_list, "", db_info['userans'], db_info['correct'], data, prefix=n))
         self.form_list.append(morph)
 
 
@@ -249,7 +252,7 @@ class NumGame(Game):
             random_num = randint(0, int(self.settings.maxnum))
 
             if random_num:
-                db_info.numeral = random_num
+                db_info['numeral'] = random_num
                 return 
 
     def create_form(self, db_info, n, data=None):
@@ -262,8 +265,8 @@ class NumGame(Game):
         fstdir="/opt/smi/" + language + "/bin"
         gen_norm_fst = fstdir + "/" + language + "-num.fst"
         
-        gen_norm_lookup = "echo " + str(db_info.numeral) + " | /usr/local/bin/lookup -flags mbTT -utf8 -d " + gen_norm_fst
-        #gen_norm_lookup = "echo " + str(db_info.numeral) + " | /Users/saara/bin/lookup -flags mbTT -utf8 -d " + gen_norm_fst
+        gen_norm_lookup = "echo " + str(db_info['numeral']) + " | /usr/local/bin/lookup -flags mbTT -utf8 -d " + gen_norm_fst
+        #gen_norm_lookup = "echo " + str(db_info['numeral']) + " | /Users/saara/bin/lookup -flags mbTT -utf8 -d " + gen_norm_fst
         print gen_norm_lookup
         num_tmp = os.popen(gen_norm_lookup).readlines()
         num_list=[]
@@ -273,7 +276,7 @@ class NumGame(Game):
                 nums = line.split('\t')
                 num_list.append(nums[1].decode('utf-8'))
         numstring = num_list[0]
-        form = (NumQuestion(db_info.numeral, numstring, num_list, self.settings.numgame, db_info.userans, db_info.correct, data, prefix=n))
+        form = (NumQuestion(db_info['numeral'], numstring, num_list, self.settings.numgame, db_info['userans'], db_info['correct'], data, prefix=n))
         self.form_list.append(form)
 
 
@@ -291,13 +294,13 @@ class QuizzGame(Game):
 
             tr_list=random_word.translation.all()
             if tr_list:
-                db_info.word_id = word_id
-                db_info.question_id = ""
+                db_info['word_id'] = word_id
+                db_info['question_id'] = ""
                 return db_info
 
     def create_form(self, db_info, n, data=None):
 
-        word_id = db_info.word_id
+        word_id = db_info['word_id']
         translations=Translationnob.objects.filter(Q(word__pk=word_id))
         word_list = Word.objects.filter(Q(id=word_id))
         question_list=[]
@@ -311,7 +314,7 @@ class QuizzGame(Game):
         #if db_info.question_id:
         #    question_list = Question.objects.filter(Q(id=db_info.question_id))
             
-        form = (QuizzQuestion(word, tr_list, question_list, db_info.userans, db_info.correct, data, prefix=n))
+        form = (QuizzQuestion(word, tr_list, question_list, db_info['userans'], db_info['correct'], data, prefix=n))
         self.form_list.append(form)
 
 
