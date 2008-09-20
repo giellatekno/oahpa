b3dd6ff473dd28bd3f6b1f29d8702fcf7bcc527f
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2008-09-20 15:26:28 +0000

    Admin interface for Log messages and database.

---
+++ oahpa/drill/admin.py
@@ -0,0 +1,17 @@
+from oahpa.drill.models import Log, Word, Semtype
+from django.contrib import admin
+from django.contrib.contenttypes import generic
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('example','userinput','iscorrect','correct','game','date')
+        
+class WordAdmin(admin.ModelAdmin):
+
+    list_display = ('lemma','wordid','pos','stem', 'diphthong','gradation','rime','soggi','dialect','valency')
+    list_filter = ['pos','stem','semtype','source'] 
+    search_fields = ['lemma', 'semtype__semtype']
+
+#admin.site.register(Word,WordAdmin)
+admin.site.register(Log, LogAdmin)
+
+
