7f625b6f4ef3f25a1052f79ceaa1c60790068b3f
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2008-10-27 16:59:06 +0000

    Added loop, quit the game by "q". End the answer with enter. Possibility to use qid for questions.

--- src/vasta.py
+++ src/vasta.py
@@ -23,6 +23,8 @@ parser.add_option("-p", "--pos", dest="pos",
                   help="part of speech")
 parser.add_option("-g", "--grammar", dest="grammar",
                   help="Grammarfile")
+parser.add_option("-i", "--id", dest="qid",
+                  help="Question id")
 parser.add_option("-q", "--qtype", dest="qtype",
                   help="question type")
 
@@ -31,6 +33,7 @@ parser.add_option("-q", "--qtype", dest="qtype",
 qasettings = {'pos' : 'N', 'qtype' : 'N-ILL', 'case' : '', 'allcase':''}
 if options.pos: qasettings['pos'] = options.pos
 if options.qtype: qasettings['qtype'] = options.qtype
+if options.qid: qasettings['qid'] = options.qid
 
 
 # Analyzer..
@@ -64,92 +67,105 @@ game = QAGame(qasettings)
 game.init_tags()
 game.gametype = 'qa'
 new_db_info = {}
-db_info = game.get_db_info(new_db_info, qasettings['qtype'])
+if qasettings.has_key('qid'):
+    db_info = game.get_db_info(new_db_info, None, qasettings['qid'])
+else:
+    db_info = game.get_db_info(new_db_info, qasettings['qtype'])
 
-question = Question.objects.get(Q(id=db_info['question_id']))
-qtext = question.string
+print "++++++"
+print "Write the answer and press enter."
+print "Quit the game with \"q\" or \"quit\" or \"exit\"."
+print "+++++++"
+contin=True
+while contin:
+    question = Question.objects.get(Q(id=db_info['question_id']))
+    qtext = question.string
+    
+    qstring =""
+    analysis = ""
+    qwords = db_info['qwords']
+    for w in qtext.split():
+        cohort=""
+        if qwords.has_key(w):
+            qword = qwords[w]
+            if qword.has_key('word'):
+                if qword.has_key('fullform') and qword['fullform']:
+                    cohort = cohort + "\"<" + qword['fullform'][0].encode('utf-8') + ">\"\n"
+                    qstring = qstring + " " + qwords[w]['fullform'][0]
 
-qstring =""
-analysis = ""
-qwords = db_info['qwords']
-for w in qtext.split():
-    cohort=""
-    if qwords.has_key(w):
-        qword = qwords[w]
-        if qword.has_key('word'):
-            if qword.has_key('fullform') and qword['fullform']:
-                cohort = cohort + "\"<" + qword['fullform'][0].encode('utf-8') + ">\"\n"
+                    lemma = Word.objects.filter(id=qword['word'])[0].lemma
+                    cohort = cohort + "\t\"" + lemma.encode('utf-8') + "\""
+                if qword.has_key('tag') and qword['tag']:
+                    string = Tag.objects.filter(id=qword['tag'])[0].string
+                    tag = string.replace("+"," ")
+                    cohort = cohort + " " + tag.encode('utf+8') + "\n"
+            else:
+                word_lookup = "echo \"" + w.encode('utf-8') + "\"" + lookup + lookup2cg
+                word = os.popen(word_lookup).readlines()
+                for c in word:
+                    c.lstrip(" ")
+                    cohort = cohort + c
                 qstring = qstring + " " + qwords[w]['fullform'][0]
-
-                lemma = Word.objects.filter(id=qword['word'])[0].lemma
-                cohort = cohort + "\t\"" + lemma.encode('utf-8') + "\""
-            if qword.has_key('tag') and qword['tag']:
-                string = Tag.objects.filter(id=qword['tag'])[0].string
-                tag = string.replace("+"," ")
-                cohort = cohort + " " + tag.encode('utf+8') + "\n"
         else:
-            word_lookup = "echo \"" + w.encode('utf-8') + "\"" + lookup + lookup2cg
-            word = os.popen(word_lookup).readlines()
-            for c in word:
-                c.lstrip(" ")
-                cohort = cohort + c
-            qstring = qstring + " " + qwords[w]['fullform'][0]
-    else:
-        qstring = qstring + " " + w
+            qstring = qstring + " " + w
         
-    if not cohort:
-        cohort = w + "\n"
+        if not cohort:
+            cohort = w + "\n"
         
-    analysis = analysis + cohort
+        analysis = analysis + cohort.decode('utf-8')
+        
+    analysis = analysis + "\"<^qst>\"\n\t\"^qst\" QDL\n"
 
-analysis = analysis + "\"<^qst>\"\n\t\"^qst\" QDL\n"
+    qstring = qstring.lstrip()
+    qstring = qstring[0].capitalize() + qstring[1:]
 
-qstring = qstring.lstrip()
-qstring = qstring[0].capitalize() + qstring[1:]
+    qstring = qstring + "?"
+    print qstring
+    data = sys.stdin.readline()
+    data = ''.join(data)
+    data = data.rstrip()
+    # quit the loop if the user wants so
+    if data == "q" or data=="quit" or data=="exit":
+        contin=False
+        break
+    
+    ans_cohort=""
+    data_lookup = "echo \"" + data + "\"" + preprocess + lookup + lookup2cg
+    #print data_lookup
+    word = os.popen(data_lookup).readlines()
+    for c in word:
+        c.lstrip(" ")
+        ans_cohort = ans_cohort + c
 
-qstring = qstring + "?"
-print qstring
-data = sys.stdin.readlines()
-data = ''.join(data)
-data = data.rstrip()
+    analysis = analysis + ans_cohort
+    #print analysis
+    analysis = analysis.rstrip()
+    analysis = analysis.replace("\"","\\\"")
 
-ans_cohort=""
-data_lookup = "echo \"" + data + "\"" + preprocess + lookup + lookup2cg
-#print data_lookup
-word = os.popen(data_lookup).readlines()
-for c in word:
-    c.lstrip(" ")
-    ans_cohort = ans_cohort + c
+    ped_cg3 = "echo \"" + analysis + "\"" + vislcg3
+    #print "***************"
+    #print ped_cg3
+    checked = os.popen(ped_cg3).readlines()
 
-analysis = analysis + ans_cohort
-#print analysis
-analysis = analysis.rstrip()
-analysis = analysis.replace("\"","\\\"")
+    messageObj=re.compile(r'^.*(?P<msgString>&[\w-]*)\s*$', re.U)
 
-ped_cg3 = "echo \"" + analysis + "\"" + vislcg3
-#print "***************"
-#print ped_cg3
-checked = os.popen(ped_cg3).readlines()
+    print
 
-messageObj=re.compile(r'^.*(?P<msgString>&[\w-]*)\s*$', re.U)
+    msgstrings = []
+    for line in checked:
+        line.strip()
+        print line
+        matchObj=messageObj.search(line)
+        if matchObj:
+            msgstring = matchObj.expand(r'\g<msgString>')
+            msgstrings.append(msgstring)
 
-print
-
-msgstrings = []
-for line in checked:
-    line.strip()
-    print line
-    matchObj=messageObj.search(line)
-    if matchObj:
-        msgstring = matchObj.expand(r'\g<msgString>')
-        msgstrings.append(msgstring)
-
-for m in msgstrings:
-    m = m.replace("&","")
-    if Feedbackmsg.objects.filter(msgid=m).count() > 0:
-        message = Feedbackmsg.objects.filter(msgid=m)[0].message
-        print message
-    else:
-        print m
+    for m in msgstrings:
+        m = m.replace("&","")
+        if Feedbackmsg.objects.filter(msgid=m).count() > 0:
+            message = Feedbackmsg.objects.filter(msgid=m)[0].message
+            print message
+        else:
+            print m
 
 
