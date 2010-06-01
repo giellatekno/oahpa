2387588c33c83a4684839194ead205a347bacdbf
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-06-01 10:36:31 +0000

    changed routine: reading from xml file

--- qa_test/00_readme.txt
+++ qa_test/00_readme.txt
@@ -3,7 +3,7 @@ This is a directory for automatizing question-answer pair testing.
 preliminary version of the script
 
 command:
-perl test_qa.pl in_data.txt
+perl test_qa.pl xyz_firstmeeting.xml
 
 output:
 tmp_data.txt (intermediate data just for check)
--- qa_test/test_qa.pl
+++ qa_test/test_qa.pl
@@ -2,57 +2,59 @@
 # Perl script to test question-answers pairs for sahka and vasta
 
 use File::Spec;
+use XML::Twig;
 
-my $word_pos_list = $ARGV[0];
+my $infile = $ARGV[0];
+#my $twig = XML::Twig->new(keep_encoding => 1);
+my $twig = XML::Twig->new();
 # to extend for vasta, too
-my $s = '\^sahka';
+my $s = '^sahka';
 my $lon = 'lookup -flags mbTT -utf8 ~/gtsvn/gt/sme/bin/sme-norm.fst';
 my $tmp_file = "tmp_data.txt";
 my $out_file = "final_data.txt";
 
-open FILE, $word_pos_list or die $!;
+$twig->parsefile($infile);
+my $root = $twig->root;
 
-while ( <FILE> ) {
-  chomp;
-  if (/^\s.*$/) {
-    next;
-  } else {
-    my ($q, $a, $i) = split(/$s/, $_);
+foreach my $test ($root->children('test')){
+  my $i =  $test->att('name');
+  $i = "QDL ".$i;
+  print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+  print "processing =>ID: $i\n";
+  my $q = $test->first_child_text('q');
+  foreach my $r ($test->children('a')){
+    my $a = $r->text();
     print "question: $q\n";
     print "answer: $a\n";
-    print "id: $i\n";
-    $i = "QDL ".$i;
-    $s = '^sahka';
+
     my $command1 = "echo '$q $s $a' | preprocess | $lon | lookup2cg |" ;
-
-#    print "$command1\n";
-
-    open (MYFILE1, ">>$tmp_file");
+    open (TMPFILE, ">>$tmp_file");
     open (CMD1, $command1);
     while (<CMD1>){
       s/^(\s+\"\^sahka\"\s+)(\?.*)$/$1$i/;
-      print MYFILE1;
+      print TMPFILE;
     }
-    print MYFILE1 "\n=========================================================\n\n";
-    print "\n=========================================================\n\n";
+    print TMPFILE "\n=========================================================\n\n";
+    print "=========================================================\n";
 
     close CMD1;
-    close (MYFILE1);     
-
-    $s = '\^sahka';
-  }    
+    close (TMPFILE);     
+  }
+  print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
 }
 
-my $command2 = "cat $tmp_file | vislcg3 -g ../sme/src/sme-ped.cg3 |" ;
+my $command2 = "cat $tmp_file | vislcg3 -g ped/src/sme-ped.cg3 |" ;
 
 #    print "$command2\n";
 
-open (MYFILE2, ">>$out_file");
+open (ENDFILE, ">>$out_file");
 
 open (CMD2, $command2);
 while (<CMD2>){
-  print MYFILE2;
+  print ENDFILE;
 }
 close CMD2;
-close (MYFILE2);     
+close (ENDFILE);     
+
+__END__
 
