ec45815456743d37021eb6eb41ef83d6bf11f826
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-06-07 20:59:21 +0000

    minor changed script for Lene's new needs

---
+++ qa_test/test_all_qa.pl
@@ -0,0 +1,62 @@
+#!/bin/perl -w
+# Perl script to test question-answers pairs for sahka and vasta
+
+use File::Spec;
+use XML::Twig;
+
+my $infile = $ARGV[0];
+#my $twig = XML::Twig->new(keep_encoding => 1);
+my $twig = XML::Twig->new();
+# to extend for vasta, too
+my $s = '^sahka';
+my $lon = 'lookup -flags mbTT -utf8 ~/gtsvn/gt/sme/bin/sme-norm.fst';
+my $tmp_file = "ftmp_data.txt";
+my $out_file = "final_data.txt";
+
+$twig->parsefile($infile);
+my $root = $twig->root;
+
+foreach my $qt ($root->children('q_tests')){
+  foreach my $test ($qt->children('test')){
+    my $i =  $test->att('name');
+    $i = "QDL ".$i;
+    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    print "processing =>ID: $i\n";
+    my $q = $test->first_child_text('q');
+    foreach my $r ($test->children('a')){
+      my $a = $r->text();
+      print "question: $q\n";
+      print "answer: $a\n";
+      
+      my $command1 = "echo '$q $s $a' | preprocess | $lon | lookup2cg |" ;
+      open (TMPFILE, ">>$tmp_file");
+      open (CMD1, $command1);
+      while (<CMD1>){
+	s/^(\s+\"\^sahka\"\s+)(\?.*)$/$1$i/;
+	print TMPFILE;
+      }
+      print TMPFILE "\n=========================================================\n\n";
+      print "=========================================================\n";
+      
+      close CMD1;
+      close (TMPFILE);     
+    }
+    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+  }
+}
+
+my $command2 = "cat $tmp_file | vislcg3 -g ped/src/sme-ped.cg3 |" ;
+
+#    print "$command2\n";
+
+open (ENDFILE, ">>$out_file");
+
+open (CMD2, $command2);
+while (<CMD2>){
+  print ENDFILE;
+}
+close CMD2;
+close (ENDFILE);     
+
+__END__
+
