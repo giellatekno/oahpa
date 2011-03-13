0ed22f6265dbbad61946b3fed3273bb8bd929904
Trond Trosterud <trond.trosterud@uit.no>
2011-03-13 23:06:20 +0000

    Corrected links, sigh.

--- doc/adjtabell.sh
+++ doc/adjtabell.sh
@@ -8,13 +8,13 @@ cat $GTHOME/gt/sma/src/adj-sma-lex.txt |tr "\!" "£"|cut -d"£" -f1|grep ";"|tr 
 
 # Printing headers:
 
-echo "!!!Tabell over adjektiv" > adjtabell.jspwiki
-echo "" >> adjtabell.jspwiki
-echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> adjtabell.jspwiki
+echo "!!!Tabell over adjektiv" > smadoc/adjtabell.jspwiki
+echo "" >> smadoc/adjtabell.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> smadoc/adjtabell.jspwiki
 
-echo "!!!Reversert tabell over adjektiv" > adjtabellrev.jspwiki
-echo "" >> adjtabellrev.jspwiki
-echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> adjtabellrev.jspwiki
+echo "!!!Reversert tabell over adjektiv" > smadoc/adjtabellrev.jspwiki
+echo "" >> smadoc/adjtabellrev.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> smadoc/adjtabellrev.jspwiki
 
 # Making the 4 columns
 cat t1|sed 's/$/+A+Attr/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tattr
@@ -27,12 +27,12 @@ cat t1|sed 's/$/+A+Superl+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n'
 
 paste -d"|" t1 tattr tpred tcomp tsuperl | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > tadjforms
 
-cat tadjforms >> adjtabell.jspwiki
+cat tadjforms >> smadoc/adjtabell.jspwiki
 
-#rm tattr tpred tcomp tsuperl
+rm tattr tpred tcomp tsuperl 
 
 cat t1 | perl -nle 'print scalar reverse $_' > t1rev
 
-paste t1rev tadjforms | sort | cut -f2 | grep '[A-Za-z]' >> adjtabellrev.jspwiki
+paste t1rev tadjforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/adjtabellrev.jspwiki
 
-#rm t1rev
+rm t1rev tadjforms
