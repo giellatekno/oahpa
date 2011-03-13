a5e3b46122458a0b5d0de91e3695107ff4d7206d
Trond Trosterud <trond.trosterud@uit.no>
2011-03-13 16:40:48 +0000

    Adj updates

--- doc/adjtabell.sh
+++ doc/adjtabell.sh
@@ -6,6 +6,16 @@ cat $GTHOME/gt/sma/src/adj-sma-lex.txt |tr "\!" "£"|cut -d"£" -f1|grep ";"|tr 
 # b. only oahpa adjectives
 # blabla
 
+# Printing headers:
+
+echo "!!!Tabell over adjektiv" > adjtabell.jspwiki
+echo "" >> adjtabell.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> adjtabell.jspwiki
+
+echo "!!!Reversert tabell over adjektiv" > adjtabellrev.jspwiki
+echo "" >> adjtabellrev.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> adjtabellrev.jspwiki
+
 # Making the 4 columns
 cat t1|sed 's/$/+A+Attr/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tattr
 
@@ -15,12 +25,14 @@ cat t1|sed 's/$/+A+Comp+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '
 
 cat t1|sed 's/$/+A+Superl+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tsuperl
 
-paste -d"|" t1 tattr tpred tcomp tsuperl | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;'> ttabell
+paste -d"|" t1 tattr tpred tcomp tsuperl | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > tadjforms
 
-rm tattr tpred tcomp tsuperl
+cat tadjforms >> adjtabell.jspwiki
+
+#rm tattr tpred tcomp tsuperl
 
 cat t1 | perl -nle 'print scalar reverse $_' > t1rev
 
-paste t1rev ttabell | sort | cut -f2 > ttabellrev
+paste t1rev tadjforms | sort | cut -f2 | grep '[A-Za-z]' >> adjtabellrev.jspwiki
 
-rm t1rev
+#rm t1rev
