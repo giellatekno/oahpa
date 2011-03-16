10897300535094b450e56aec1e71024c679fd1b9
Trond Trosterud <trond.trosterud@uit.no>
2011-03-16 21:24:04 +0000

    dictadjs

--- doc/adjtabell.sh
+++ doc/adjtabell.sh
@@ -1,20 +1,24 @@
 # list of command making jspwiki document
 
 # a. all adjectives
-cat $GTHOME/gt/sma/src/adj-sma-lex.txt |tr "\!" "£"|cut -d"£" -f1|grep ";"|tr '[+:]' ' '| cut -d" " -f1|tr -d '[#^]'|sort|uniq > t1
+#cat $GTHOME/gt/sma/src/adj-sma-lex.txt |tr "\!" "£"|cut -d"£" -f1|grep ";"|tr '[+:]' ' '| cut -d" " -f1|tr -d '[#^]'|sort|uniq > t1
+
+# b. dict adjs
+cat ../../words/dicts/smanob/src/a_smanob.xml|grep '<l '|tr '<' '>'|cut -d">" -f3 > t1
+doc$
 
 # b. only oahpa adjectives
 # blabla
 
 # Printing headers:
 
-echo "!!!Tabell over adjektiv" > smadoc/adjtabell.jspwiki
-echo "" >> smadoc/adjtabell.jspwiki
-echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> smadoc/adjtabell.jspwiki
+echo "!!!Tabell over adjektiv" > smadoc/adjdicttabell.jspwiki
+echo "" >> smadoc/adjdicttabell.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> smadoc/adjdicttabell.jspwiki
 
-echo "!!!Reversert tabell over adjektiv" > smadoc/adjtabellrev.jspwiki
-echo "" >> smadoc/adjtabellrev.jspwiki
-echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> smadoc/adjtabellrev.jspwiki
+echo "!!!Reversert tabell over adjektiv" > smadoc/adjdicttabellrev.jspwiki
+echo "" >> smadoc/adjdicttabellrev.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  ||  Comp  ||  Superl " >> smadoc/adjdicttabellrev.jspwiki
 
 # Making the 4 columns
 cat t1|sed 's/$/+A+Attr/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tattr
@@ -27,12 +31,12 @@ cat t1|sed 's/$/+A+Superl+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n'
 
 paste -d"|" t1 tattr tpred tcomp tsuperl | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > tadjforms
 
-cat tadjforms >> smadoc/adjtabell.jspwiki
+cat tadjforms >> smadoc/adjdicttabell.jspwiki
 
-rm tattr tpred tcomp tsuperl 
+#rm tattr tpred tcomp tsuperl 
 
 cat t1 | perl -nle 'print scalar reverse $_' > t1rev
 
-paste t1rev tadjforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/adjtabellrev.jspwiki
+paste t1rev tadjforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/adjdicttabellrev.jspwiki
 
-rm t1rev tadjforms
+#rm t1rev tadjforms
