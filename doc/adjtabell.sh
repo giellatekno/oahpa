fb219350686688fb0bc13dfd62c862834afa171d
Trond Trosterud <trond.trosterud@uit.no>
2011-03-18 19:55:52 +0000

    Update

--- doc/adjtabell.sh
+++ doc/adjtabell.sh
@@ -4,34 +4,38 @@
 #cat $GTHOME/gt/sma/src/adj-sma-lex.txt |tr "\!" "£"|cut -d"£" -f1|grep ";"|tr '[+:]' ' '| cut -d" " -f1|tr -d '[#^]'|sort|uniq > t1
 
 # b. dict adjs
-cat ../../words/dicts/smanob/src/a_smanob.xml|grep '<l '|tr '<' '>'|cut -d">" -f3 > t1
+#cat ../../words/dicts/smanob/src/a_smanob.xml|grep '<l '|tr '<' '>'|cut -d">" -f3 > t1
 
 
 # c. only oahpa adjectives
-# blabla
+# cat ../../words/dicts/smanob/src/a_smanob.xml |tr '\n' '™' | sed 's/<l /£/g;'| tr '£' '\n'|grep '"oahpa"'|tr '™' '\n' |grep '^pos'|tr '<' '>' | cut -d">" -f2|sort|uniq > t1
+
 
 # Printing headers:
 
-echo "!!!Tabell over adjektiv - OBS! arbeidsliste for debugging" > smadoc/adjdicttabell.jspwiki
-echo "" >> smadoc/adjdicttabell.jspwiki
-echo " ||  lemma  ||  Attr  ||  Pred  || Sg Nom || Comp Pred || Comp Sg Nom || Superl Pred || Superl Sg Nom " >> smadoc/adjdicttabell.jspwiki
+echo "!!!Tabell over adjektiv - OBS! arbeidsliste for debugging" > smadoc/adjoahpatabell.jspwiki
+echo "" >> smadoc/adjoahpatabell.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  || Sg Nom || Comp Pred || Comp Sg Nom || Superl Pred || Superl Sg Nom " >> smadoc/adjoahpatabell.jspwiki
 
-echo "!!!Reversert tabell over adjektiv - OBS! arbeidsliste for debugging" > smadoc/adjdicttabellrev.jspwiki
-echo "" >> smadoc/adjdicttabellrev.jspwiki
-echo " ||  lemma  ||  Attr  ||  Pred  || Sg Nom || Comp Pred || Comp Sg Nom || Superl Pred || Superl Sg Nom " >> smadoc/adjdicttabellrev.jspwiki
+echo "!!!Reversert tabell over adjektiv - OBS! arbeidsliste for debugging" > smadoc/adjoahpatabellrev.jspwiki
+echo "" >> smadoc/adjoahpatabellrev.jspwiki
+echo " ||  lemma  ||  Attr  ||  Pred  || Sg Nom || Comp Pred || Comp Sg Nom || Superl Pred || Superl Sg Nom " >> smadoc/adjoahpatabellrev.jspwiki
 
 # Making the 4 columns
 cat t1|sed 's/$/+A+Attr/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tattr
 
 #cat t1|sed 's/$/+A+Pred/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tpred
+#cat t1|sed 's/$/+A+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tpred
 
 cat t1|sed 's/$/+A+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tsgnom
 
 #cat t1|sed 's/$/+A+Comp+Pred/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tcomppred
+#cat t1|sed 's/$/+A+Comp+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tcomppred
 
 cat t1|sed 's/$/+A+Comp+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tcomp
 
 #cat t1|sed 's/$/+A+Superl+Pred/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tsuperlpred
+#cat t1|sed 's/$/+A+Superl+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tsuperlpred
 
 cat t1|sed 's/$/+A+Superl+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+A.*/-/'|tr '\t' ',' > tsuperl
 
@@ -40,12 +44,12 @@ cat t1|sed 's/$/+A+Superl+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma.fst |tr '\n'
 # without pred tags:
 paste -d"|" t1 tattr tsgnom tcomp tsuperl | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > tadjforms
 
-cat tadjforms >> smadoc/adjdicttabell.jspwiki
+cat tadjforms >> smadoc/adjoahpatabell.jspwiki
 
 rm tattr tpred tsgnom tcomppred tcomp tsuperlpred  tsuperl 
 
 cat t1 | perl -nle 'print scalar reverse $_' > t1rev
 
-paste t1rev tadjforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/adjdicttabellrev.jspwiki
+paste t1rev tadjforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/adjoahpatabellrev.jspwiki
 
 rm t1rev tadjforms
