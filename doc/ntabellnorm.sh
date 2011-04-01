923a72602e020cf8d6eb9b75e0939d39766532ec
Trond Trosterud <trond.trosterud@uit.no>
2011-04-01 09:19:37 +0000

    norms

---
+++ doc/ntabellnorm.sh
@@ -0,0 +1,90 @@
+# list of command making jspwiki document
+
+# a. all nouns
+cat $GTHOME/gt/sma/src/noun-sma-lex.txt |tr "\!" "£"|cut -d"£" -f1|grep ";"|tr '[+:]' ' '| cut -d" " -f1|tr -d '[#^]'|sort|uniq > t1
+
+# b. dict nouns
+ cat ../../words/dicts/smanob/src/n_smanob.xml|grep '<l '|tr '<' '>'|cut -d">" -f3 > t2
+
+
+# c. only oahpa nouns
+ cat ../../words/dicts/smanob/src/n_smanob.xml |tr '\n' '™' | sed 's/<l /£/g;'| tr '£' '\n'|grep '"oahpa"'|tr '™' '\n' |grep '^pos'|tr '<' '>' | cut -d">" -f2|sort|uniq > t3
+
+
+# Printing headers:
+
+echo "!!!Tabell over alle substantiv - OBS! arbeidsliste for debugging" > smadoc/gen/nounalletabellnorm.jspwiki
+echo "" >> smadoc/gen/nounalletabellnorm.jspwiki
+echo " ||  lemma  ||  Nom Sg  ||  Gen Sg || Ill Sg || Ine Sg || Ess || Nom Pl || Acc Pl || Ill Pl " >> smadoc/gen/nounalletabellnorm.jspwiki
+echo "!!!Reversert tabell over alle substantiv - OBS! arbeidsliste for debugging" > smadoc/gen/nounalletabellrevnorm.jspwiki
+echo "" >> smadoc/gen/nounalletabellrevnorm.jspwiki
+echo " ||  lemma  ||  Nom Sg  ||  Gen Sg || Ill Sg || Ine Sg || Ess || Nom Pl || Acc Pl || Ill Pl " >> smadoc/gen/nounalletabellrevnorm.jspwiki
+
+echo "!!!Tabell over dict-substantiv - OBS! arbeidsliste for debugging" > smadoc/gen/noundicttabellnorm.jspwiki
+echo "" >> smadoc/gen/noundicttabellnorm.jspwiki
+echo " ||  lemma  ||  Nom Sg  ||  Gen Sg || Ill Sg || Ine Sg || Ess || Nom Pl || Acc Pl || Ill Pl " >> smadoc/gen/noundicttabellnorm.jspwiki
+echo "!!!Reversert tabell over dict-substantiv - OBS! arbeidsliste for debugging" > smadoc/gen/noundicttabellrevnorm.jspwiki
+echo "" >> smadoc/gen/noundicttabellrevnorm.jspwiki
+echo " ||  lemma  ||  Nom Sg  ||  Gen Sg || Ill Sg || Ine Sg || Ess || Nom Pl || Acc Pl || Ill Pl " >> smadoc/gen/noundicttabellrevnorm.jspwiki
+
+echo "!!!Tabell over oahpa-substantiv - OBS! arbeidsliste for debugging" > smadoc/gen/nounoahpatabellnorm.jspwiki
+echo "" >> smadoc/gen/nounoahpatabellnorm.jspwiki
+echo " ||  lemma  ||  Nom Sg  ||  Gen Sg || Ill Sg || Ine Sg || Ess || Nom Pl || Acc Pl || Ill Pl " >> smadoc/gen/nounoahpatabellnorm.jspwiki
+echo "!!!Reversert tabell over oahpa-substantiv - OBS! arbeidsliste for debugging" > smadoc/gen/nounoahpatabellrevnorm.jspwiki
+echo "" >> smadoc/gen/nounoahpatabellrevnorm.jspwiki
+echo " ||  lemma  ||  Nom Sg  ||  Gen Sg || Ill Sg || Ine Sg || Ess || Nom Pl || Acc Pl || Ill Pl " >> smadoc/gen/nounoahpatabellrevnorm.jspwiki
+
+# Making the 7 columns
+cat t1|sed 's/$/+N+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1sgnom
+cat t2|sed 's/$/+N+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2sgnom
+cat t3|sed 's/$/+N+Sg+Nom/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3sgnom
+
+cat t1|sed 's/$/+N+Sg+Gen/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1sggen
+cat t2|sed 's/$/+N+Sg+Gen/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2sggen
+cat t3|sed 's/$/+N+Sg+Gen/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3sggen
+
+cat t1|sed 's/$/+N+Sg+Ill/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1sgill
+cat t2|sed 's/$/+N+Sg+Ill/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2sgill
+cat t3|sed 's/$/+N+Sg+Ill/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3sgill
+
+cat t1|sed 's/$/+N+Sg+Ine/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1sgine
+cat t2|sed 's/$/+N+Sg+Ine/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2sgine
+cat t3|sed 's/$/+N+Sg+Ine/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3sgine
+
+cat t1|sed 's/$/+N+Ess/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1ess
+cat t2|sed 's/$/+N+Ess/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2ess
+cat t3|sed 's/$/+N+Ess/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3ess
+
+cat t1|sed 's/$/+N+Pl+Nom/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1plnom
+cat t2|sed 's/$/+N+Pl+Nom/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2plnom
+cat t3|sed 's/$/+N+Pl+Nom/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3plnom
+
+cat t1|sed 's/$/+N+Pl+Acc/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1placc
+cat t2|sed 's/$/+N+Pl+Acc/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2placc
+cat t3|sed 's/$/+N+Pl+Acc/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3placc
+
+cat t1|sed 's/$/+N+Pl+Ill/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t1plill
+cat t2|sed 's/$/+N+Pl+Ill/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t2plill
+cat t3|sed 's/$/+N+Pl+Ill/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+N.*/-/'|tr '\t' ',' > t3plill
+
+
+
+paste -d"|" t1 t1sggen t1sgill t1sgine t1ess t1plnom t1placc t1plill | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > t1nounforms
+paste -d"|" t2 t2sggen t2sgill t2sgine t2ess t2plnom t2placc t2plill | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > t2nounforms
+paste -d"|" t3 t3sggen t3sgill t3sgine t3ess t3plnom t3placc t3plill | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > t3nounforms
+
+cat t1nounforms >> smadoc/gen/nounalletabellnorm.jspwiki
+cat t2nounforms >> smadoc/gen/noundicttabellnorm.jspwiki
+cat t3nounforms >> smadoc/gen/nounoahpatabellnorm.jspwiki
+
+rm -f  t?sggen t?sgill t?sgine t?ess t?plnom t?placc t?plill 
+
+cat t1 | perl -nle 'print scalar reverse $_' > t1rev
+cat t2 | perl -nle 'print scalar reverse $_' > t2rev
+cat t3 | perl -nle 'print scalar reverse $_' > t3rev
+
+paste t1rev t1nounforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/gen/nounalletabellrevnorm.jspwiki
+paste t2rev t2nounforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/gen/noundicttabellrevnorm.jspwiki
+paste t3rev t3nounforms | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/gen/nounoahpatabellrevnorm.jspwiki
+
+rm t1 t2 t3 t1rev t2rev t3rev t?nounforms
---
+++ doc/vtabellnorm.sh
@@ -0,0 +1,93 @@
+# list of command making jspwiki document
+
+
+# a. all verbs
+cat $GTHOME/gt/sma/src/verb-sma-lex.txt |tr "\!" "£"|cut -d"£" -f1|grep ";"|tr '[+:]' ' '| cut -d" " -f1|tr -d '[#^]'|sort|uniq > t1
+
+# b. dict verbs
+ cat ../../words/dicts/smanob/src/v_smanob.xml|grep '<l '|tr '<' '>'|cut -d">" -f3 > t2
+
+
+# c. only oahpa verbs
+ cat ../../words/dicts/smanob/src/v_smanob.xml |tr '\n' '™' | sed 's/<l /£/g;'| tr '£' '\n'|grep '"oahpa"'|tr '™' '\n' |grep '^pos'|tr '<' '>' | cut -d">" -f2|sort|uniq > t3
+
+
+# Printing headers:
+
+echo "!!!Tabell over alle verb - OBS! arbeidsliste for debugging" > smadoc/gen/verballetabellnorm.jspwiki
+echo "" >> smadoc/gen/verballetabellnorm.jspwiki
+echo " ||  lemma  ||  Inf  ||  Prs Sg1 || Prs Sg3 || Prs Du1 || Prs Du3 || Prs Pl3 || Prt Sg1 || Ger || PrsPrc " >> smadoc/gen/verballetabellnorm.jspwiki
+echo "!!!Reversert tabell over alle verb - OBS! arbeidsliste for debugging" > smadoc/gen/verballetabellrevnorm.jspwiki
+echo "" >> smadoc/gen/verballetabellrevnorm.jspwiki
+echo " ||  lemma  ||  Inf  ||  Prs Sg1 || Prs Sg3 || Prs Du1 || Prs Du3 || Prs Pl3 || Prt Sg1 || Ger || PrsPrc " >> smadoc/gen/verballetabellrevnorm.jspwiki
+
+echo "!!!Tabell over dict-verb - OBS! arbeidsliste for debugging" > smadoc/gen/verbdicttabellnorm.jspwiki
+echo "" >> smadoc/gen/verbdicttabellnorm.jspwiki
+echo " ||  lemma  ||  Inf  ||  Prs Sg1 || Prs Sg3 || Prs Du1 || Prs Du3 || Prs Pl3 || Prt Sg1 || Ger || PrsPrc " >> smadoc/gen/verbdicttabellnorm.jspwiki
+echo "!!!Reversert tabell over dict-verb - OBS! arbeidsliste for debugging" > smadoc/gen/verbdicttabellrevnorm.jspwiki
+echo "" >> smadoc/gen/verbdicttabellrevnorm.jspwiki
+echo " ||  lemma  ||  Inf  ||  Prs Sg1 || Prs Sg3 || Prs Du1 || Prs Du3 || Prs Pl3 || Prt Sg1 || Ger || PrsPrc " >> smadoc/gen/verbdicttabellrevnorm.jspwiki
+
+echo "!!!Tabell over oahpa-verb - OBS! arbeidsliste for debugging" > smadoc/gen/verboahpatabellnorm.jspwiki
+echo "" >> smadoc/gen/verboahpatabellnorm.jspwiki
+echo " ||  lemma  ||  Inf  ||  Prs Sg1 || Prs Sg3 || Prs Du1 || Prs Du3 || Prs Pl3 || Prt Sg1 || Ger || PrsPrc " >> smadoc/gen/verboahpatabellnorm.jspwiki
+echo "!!!Reversert tabell over oahpa-verb - OBS! arbeidsliste for debugging" > smadoc/gen/verboahpatabellrevnorm.jspwiki
+echo "" >> smadoc/gen/verboahpatabellrevnorm.jspwiki
+echo " ||  lemma  ||  Inf  ||  Prs Sg1 || Prs Sg3 || Prs Du1 || Prs Du3 || Prs Pl3 || Prt Sg1 || Ger || PrsPrc " >> smadoc/gen/verboahpatabellrevnorm.jspwiki
+
+# Making the 7 columns
+cat t1|sed 's/$/+V+Inf/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1inf
+cat t2|sed 's/$/+V+Inf/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2inf
+cat t3|sed 's/$/+V+Inf/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3inf
+
+cat t1|sed 's/$/+V+Ind+Prs+Sg1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1prssg1
+cat t2|sed 's/$/+V+Ind+Prs+Sg1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2prssg1
+cat t3|sed 's/$/+V+Ind+Prs+Sg1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3prssg1
+
+cat t1|sed 's/$/+V+Ind+Prs+Sg3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1prssg3
+cat t2|sed 's/$/+V+Ind+Prs+Sg3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2prssg3
+cat t3|sed 's/$/+V+Ind+Prs+Sg3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3prssg3
+
+cat t1|sed 's/$/+V+Ind+Prs+Du1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1prsdu1
+cat t2|sed 's/$/+V+Ind+Prs+Du1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2prsdu1
+cat t3|sed 's/$/+V+Ind+Prs+Du1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3prsdu1
+
+cat t1|sed 's/$/+V+Ind+Prs+Du3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1prsdu3
+cat t2|sed 's/$/+V+Ind+Prs+Du3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2prsdu3
+cat t3|sed 's/$/+V+Ind+Prs+Du3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3prsdu3
+
+cat t1|sed 's/$/+V+Ind+Prs+Pl3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1prspl3
+cat t2|sed 's/$/+V+Ind+Prs+Pl3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2prspl3
+cat t3|sed 's/$/+V+Ind+Prs+Pl3/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3prspl3
+
+cat t1|sed 's/$/+V+Ind+Prt+Sg1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1prtsg1
+cat t2|sed 's/$/+V+Ind+Prt+Sg1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2prtsg1
+cat t3|sed 's/$/+V+Ind+Prt+Sg1/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3prtsg1
+
+cat t1|sed 's/$/+V+Ger/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1ger
+cat t2|sed 's/$/+V+Ger/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2ger
+cat t3|sed 's/$/+V+Ger/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3ger
+
+cat t1|sed 's/$/+V+PrsPrc/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t1prsprc
+cat t2|sed 's/$/+V+PrsPrc/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t2prsprc
+cat t3|sed 's/$/+V+PrsPrc/;'| lookup $GTHOME/gt/sma/bin/isma-norm.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8|sed 's/.*+V.*/-/'|tr '\t' ',' > t3prsprc
+
+paste -d"|" t1 t1inf t1prssg1 t1prssg3 t1prsdu1 t1prsdu3 t1prspl3 t1prtsg1 t1ger t1prsprc | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > tverbforms1
+paste -d"|" t2 t2inf t2prssg1 t2prssg3 t1prsdu1 t1prsdu3 t2prspl3 t2prtsg1 t2ger t2prsprc | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > tverbforms2
+paste -d"|" t3 t3inf t3prssg1 t3prssg3 t1prsdu1 t1prsdu3 t3prspl3 t3prtsg1 t3ger t3prsprc | sed 's/|/ | /g;' | sed 's/^/|/'| sed 's/,/, /g;' | grep '[A-Za-z]' > tverbforms3
+
+cat tverbforms1 >> smadoc/gen/verballetabellnorm.jspwiki
+cat tverbforms2 >> smadoc/gen/verbdicttabellnorm.jspwiki
+cat tverbforms3 >> smadoc/gen/verboahpatabellnorm.jspwiki
+
+rm -f t?inf t?prssg1 t?prssg3 t?prsdu1 t?prsdu3 t?prspl3 t?prtsg1 t?ger t?prsprc
+
+cat t1 | perl -nle 'print scalar reverse $_' > t1rev
+cat t2 | perl -nle 'print scalar reverse $_' > t2rev
+cat t3 | perl -nle 'print scalar reverse $_' > t3rev
+
+paste t1rev tverbforms1 | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/gen/verballetabellrevnorm.jspwiki
+paste t2rev tverbforms2 | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/gen/verbdicttabellrevnorm.jspwiki
+paste t3rev tverbforms3 | sort | cut -f2 | grep '[A-Za-z]' >> smadoc/gen/verboahpatabellrevnorm.jspwiki
+
+rm t1 t2 t3 t1rev t2rev t3rev tverbforms*
