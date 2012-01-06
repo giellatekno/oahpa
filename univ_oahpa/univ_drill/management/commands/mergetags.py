98ddb8d599dcea1950158674e1bcacae817a183e
Ryan Johnson <ryan.txanson@gmail.com>
2012-01-06 23:38:58 +0000

    Option to merge all tag duplicates

--- univ_oahpa/univ_drill/management/commands/mergetags.py
+++ univ_oahpa/univ_drill/management/commands/mergetags.py
@@ -32,6 +32,23 @@ def merge(queryset):
 #
 # # #
 
+def mergetags(tfilter=False):
+	if tfilter:
+		qset = Tag.objects.filter(string=tfilter)
+	else:
+		qset = Tag.objects.all()
+	
+	strings = qset.values_list('string', flat=True)
+	strings = list(set(strings))
+
+	print 'Merging:'
+	for string in strings:
+		tag = Tag.objects.filter(string=string)
+
+		if tag.count() > 1:
+			print 'Merging conflict in %s' % tag[0].string
+			merge(tag)
+	
 
 class Command(BaseCommand):
 	args = '--tagelement'
@@ -39,7 +56,7 @@ class Command(BaseCommand):
 	Strips tags of an element and then merges them all.
 	"""
 	option_list = BaseCommand.option_list + (
-		make_option("-t", "--tagelement", dest="tagelement", default="False",
+		make_option("-t", "--tagelement", dest="tagelement", default=False,
 						  help="Tag element to search for"),
 		make_option("-d", "--dryrun", dest="dryrun", default="True",
 						  help="List tags matching element instead of merging"),
@@ -53,22 +70,25 @@ class Command(BaseCommand):
 		tag_element = options['tagelement']
 		dry_run = options['dryrun']
 		
-		TVs = Tag.objects.filter(string__contains=tag_element)
-		
-		for TV in TVs:
-			new_str = TV.string.replace(tag_element, '')
-			filtered = Tag.objects.filter(string__contains=new_str)
+		if tag_element:
+			TVs = Tag.objects.filter(string__contains=tag_element)
 			
+			for TV in TVs:
+				new_str = TV.string.replace(tag_element, '')
+				filtered = Tag.objects.filter(string__contains=new_str)
+				
+				print 'Merging:'
+				for t in filtered:
+					print ' %s' % t.string
+				
+				filtered_up = filtered.update(string=new_str)
+				merge(filtered)
+			   
 			
-			print 'Merging:'
-			for t in filtered:
-				print ' %s' % t.string
-			
-			filtered_up = filtered.update(string=new_str)
-			merge(filtered)
-		   
-		
-		ts = Tag.objects.filter(string__contains='V+Inf')
-		ts.update(string='V+Inf')
+			ts = Tag.objects.filter(string__contains='V+Inf')
+			ts.update(string='V+Inf')
+		else:
+			mergetags()
+
 
 
