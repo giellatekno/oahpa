c658b2594bba004b6cda2ecd35b548f0f2bd01bd
Ryan Johnson <ryan.txanson@gmail.com>
2012-01-18 04:59:40 +0000

    one thing off the todo list...

--- univ_oahpa/extern/django_openid_provider/openid_provider/urls.py
+++ univ_oahpa/extern/django_openid_provider/openid_provider/urls.py
@@ -4,14 +4,12 @@ from django.conf.urls.defaults import *
 # http://oahpa.uit.no:8080/univ_oahpa/openid/ryan
 # http://oahpa.uit.no/univ_oahpa/openid/ryan
 
+# TODO: oahpa or wordpress are doing weird things with usernames in WP, which get set to a url.
+#       need to fix this 
+
 # TODO: make a shorter URL that is easier for users, or create a separate
 # django service that has access to database tables to check for user
 # permissions
-
-# TODO: need to automatically grant permission to WP when new users are created within Oahpa, 
-# thus there needs to be a method in signals.py that will automatically grant access... This way users
-# aren't interrupted for too many additional steps when they log in to WP.
-# http://129.242.218.43/wordpress/
 
 # TODO: make the permission check templates prettier / CSS
 
