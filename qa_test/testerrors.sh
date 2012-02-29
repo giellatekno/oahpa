5e85728bfea2d01277eb771c24a94e4882426294
leneantonsen <lene.antonsen@uit.no>
2012-02-29 16:47:34 +0000

    Skript for å finne fram de svarene som ikke gir error-tag etter å ha kjørt qa-test_dialogue.pl på errortest-fil.

---
+++ qa_test/testerrors.sh
@@ -0,0 +1,4 @@
+# finner fram de svarene som ikke gir error-tag etter å ha kjørt qa-test_dialogue.pl på errortest-fil
+
+cat final_data.txt | grep -v "^;" | sed 's/\^cealkka"/¢/' | tr "\n" "€" | tr -s "=" | tr "=" "\n" | cut -d "¢" -f2 | egrep -v "&" | see
+
