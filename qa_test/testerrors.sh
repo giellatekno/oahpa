b7f62f1e41edd823a3be95cd6775802ab43290b2
leneantonsen <lene.antonsen@uit.no>
2012-03-02 14:05:53 +0000

    Tilpassing

--- qa_test/testerrors.sh
+++ qa_test/testerrors.sh
@@ -1,4 +1,5 @@
 # finner fram de svarene som ikke gir error-tag etter å ha kjørt qa-test_dialogue.pl på errortest-fil
 
-cat final_data.txt | grep -v "^;" | sed 's/\^vastas"/¢/' | tr "\n" "€" | tr -s "=" | tr "=" "\n" | cut -d "¢" -f2 | egrep -v "&" | see
+cat final_data.txt | grep -v "^;" | sed 's/^$/¥/' | sed 's/\^vastas"/¢/' | tr "\n" "€" | tr "¥" "\n" | cut -d "¢" -f2 | egrep -v "&" | egrep '[a-zA-Z]' | see
+
 
