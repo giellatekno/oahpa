f9524a5ae40f3a617c3ffed6934957740bb51244
leneantonsen <lene.antonsen@uit.no>
2012-03-04 12:52:36 +0000

    Retting

--- qa_test/testerrors.sh
+++ qa_test/testerrors.sh
@@ -1,5 +1,5 @@
 # finner fram de svarene som ikke gir error-tag etter å ha kjørt qa-test_dialogue.pl på errortest-fil
 
-cat final_data.txt | grep -v "^;" | sed 's/^$/¥/' | sed 's/\^.a.*"/¢/' | tr "\n" "€" | tr "¥" "\n" | cut -d "¢" -f2 | egrep -v "&" | egrep '[a-zA-Z]' | less
+cat final_data.txt | grep -v "^;" | sed 's/^$/¥/' | sed 's/\^.a.*"/¢/' | tr "\n" "€" | tr "¥" "\n" | cut -d "¢" -f2- | egrep -v "&" | egrep '[a-zA-Z]' | less
 
 
