ae16f49ab0e7c7acb641bb808fbaef4e15d9c3ea
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2012-03-13 15:33:07 +0000

    version for vastas: either input structure would be the same for vastas vs. rest or the code would take differences in input into account, Lene skal bestemme

---
+++ qa_test/qa-test_dialogue_vasta-changes.pl
@@ -0,0 +1,104 @@
+#!/opt/local/bin/perl -w
+# Perl script to test question-answers pairs for sahka and vasta
+
+use File::Spec;
+use XML::Twig;
+
+my $infile = $ARGV[0];
+my $mode = 'sahka';
+if ($ARGV[1] ne "")
+{
+  $mode = $ARGV[1];
+  print "mode is $mode\n";
+}
+
+#my $twig = XML::Twig->new(keep_encoding => 1);
+my $twig = XML::Twig->new();
+# to extend for vasta, too
+my $s = '^'.$mode;
+if (($mode eq "sahka") or ($mode eq "vastas"))
+{
+  $s = '^'.$mode;
+} else {
+  $s = '^qst';
+}
+
+my $lon = 'lookup -flags mbTT -utf8 ./bin/ped-sme.fst';
+my $tmp_file = "tmp_data.txt";
+my $out_file = "final_data.txt";
+my $command0 = "rm $tmp_file $out_file" ;
+system ($command0) ;
+
+$twig->parsefile($infile);
+my $root = $twig->root;
+
+foreach my $test ($root->children('test')){
+  my $i =  $test->att('name');
+  $i = "QDL ".$i;
+  print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+  print "processing =>ID: $i\n";
+  my $q = $test->first_child_text('q');
+  foreach my $r ($test->children('ag')){
+    my $a = $r->first_child('a')->text();
+    print "question: $q\n";
+    print "s: $s\n";
+    print "answer: $a\n";
+
+    my $command1 = "echo '$q $s $a' | preprocess | $lon | lookup2cg | " ;
+    open (TMPFILE, ">>$tmp_file");
+    open (CMD1, $command1);
+    while (<CMD1>){
+      #s/^(\s+\"\^sahka\"\s+)(\?.*)$/$1$i/;
+      if ($mode eq "sahka")
+	{
+	  s/^(\s+\"\^sahka\"\s+)(\?.*)$/$1$i/;
+	}
+      
+      if ($mode eq "vastas")
+	{
+	  if ($_ =~ m/^(\s+\"\^vastas\"\s+)(\?.*)$/) {
+	    s/^(\s+\"\^vastas\"\s+)(\?.*)$/$1$i/;
+	    print TMPFILE;
+	     foreach my $l ($r->children('l')){
+	       print TMPFILE "         \"$l->{'att'}->{'base'}\" $l->{'att'}->{'pos'}\n";
+	      
+	       # my $b = $l->att->{'base'};
+	       # my $p = $l->att->{'pos'};
+	     }
+	  } 
+	  #s/^(\s+\"\^vastas\"\s+)(\?.*)$/$1$i/;
+	}
+
+      if ($mode eq "vastaf")
+	{
+	  s/^(\s+\"\^qst\"\s+)(\?.*)$/$1$i/;
+	}
+      if ($_ !~ m/^(\s+\"\^vastas\"\s+)(.*)$/)
+	{
+	  print TMPFILE;
+	}
+    }
+    print TMPFILE "\n\"<\.>\"\n\n";
+    print "=========================================================\n";
+
+    close CMD1;
+    close (TMPFILE);     
+  }
+  print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+}
+
+# my $command2 = "cat $tmp_file | vislcg3 -g ../sme/src/sme-ped.cg3 --trace |" ;
+
+# #    print "$command2\n";
+
+# open (ENDFILE, ">>$out_file");
+
+# open (CMD2, $command2);
+# while (<CMD2>){
+#   print ENDFILE;
+# }
+# close CMD2;
+# close (ENDFILE);     
+
+__END__
+
