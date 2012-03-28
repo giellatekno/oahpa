251a9758a832f4e4ac1e1a1e0183b4582e4777aa
leneantonsen <lene.antonsen@uit.no>
2012-03-28 10:22:28 +0000

    Endringer

--- qa_test/testsahka.sh
+++ qa_test/testsahka.sh
@@ -1,5 +1,15 @@
 # finner fram de svarene som ikke gir error-tag etter å ha kjørt qa-test_dialogue.pl på errortest-fil
 
-cat final_data.txt | grep -v "^;" | sed 's/^$/¥/' | tr "\n" "€" | tr "¥" "\n" | egrep -v "&(grm|orth|err|sem)" | egrep '[a-zA-Z]' | tr "€" "\n" | less
-
+perl qa-test_dialogue.pl regressiontests/grocery_corr.xml sahka
+cp final_data.txt  sahkafinal_data.txt
+perl qa-test_dialogue.pl regressiontests/shopadj_corr.xml sahka
+cat final_data.txt >> sahkafinal_data.txt
+perl qa-test_dialogue.pl regressiontests/visit_corr.xml sahka
+cat final_data.txt >> sahkafinal_data.txt
+perl qa-test_dialogue.pl regressiontests/firstmeeting_all_corr.xml sahka
+cat final_data.txt >> sahkafinal_data.txt
+grep -v ";" sahkafinal_data.txt | grep "&" | grep -v dia > sahkatestrapport.txt
+perl qa-test_dialogue.pl regressiontests/grocerytest.xml sahka
+grep -v "^;" final_data.txt | sed 's/^$/¥/' | tr "\n" "€" | tr "¥" "\n" | egrep -v "&(grm|orth|err|sem)" | egrep '[a-zA-Z]' | tr "€" "\n" >> sahkatestrapport.txt
+less sahkatestrapport.txt
 
