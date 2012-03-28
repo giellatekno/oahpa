cbbd2e1d374674544a8e4adcd049dfec548eb205
leneantonsen <lene.antonsen@uit.no>
2012-03-28 12:55:21 +0000

    Bedre tester

--- qa_test/testvastas.sh
+++ qa_test/testvastas.sh
@@ -1,5 +1,9 @@
 # finner fram de svarene som ikke gir error-tag etter å ha kjørt qa-test_dialogue.pl på errortest-fil
-
-grep -v "^;" final_data.txt | sed 's/^$/¥/' | tr "\n" "€" | tr "¥" "\n" | egrep -v "&(grm|orth|err|sem)" | egrep '[a-zA-Z]' | tr "€" "\n" | see
-
-
+echo "correct test" > vastatestrapport.txt
+perl qa-test_vastas.pl regressiontests/vastas_corr.xml
+grep -v ";" final_data.txt | grep "&" >> vastatestrapport.txt
+echo " " >> vastatestrapport.txt
+echo "error test" >> vastatestrapport.txt
+perl qa-test_vastas.pl errortests/vastastest.xml
+grep -v "^;" final_data.txt | sed 's/^$/¥/' | tr "\n" "€" | tr "¥" "\n" | egrep -v "&(grm|orth|err|sem)" | egrep '[a-zA-Z]' | tr "€" "\n" >> vastatestrapport.txt
+less vastatestrapport.txt
