7debf59f0491a019290eace6af8e7272c764c855
Ryan Johnson <ryan.txanson@gmail.com>
2012-04-04 21:06:49 +0000

    wordclass back, capitalize first letter

--- univ_oahpa/ling.py
+++ univ_oahpa/ling.py
@@ -262,21 +262,19 @@ class Paradigm:
 		# the first tag element, which should be the part of speech.
 		# If hid is defined simultaneously, this should not mess with that.
 
-		# No wordclass for now.
-		# if not wordtype.strip():
-		# 	wordtype = ""
-		# else:
-		# 	wordtype = '+' + wordtype.capitalize()
+		if not wordtype.strip():
+			wordtype = ""
+		else:
+			wordtype = '+' + wordtype.capitalize()
 
 		if self.paradigms.has_key(pos):
 			for a in self.paradigms[pos]:
-				# No wordclass for now.
-				# if wordtype.strip():
-					# if not wordtype in a:
-						# _pos, _, _rest = a.partition('+')
-						# tag = "%s%s+%s" % (_pos, wordtype, _rest)
-				# else:
-				tag = a
+				if wordtype.strip():
+					if not wordtype in a:
+						_pos, _, _rest = a.partition('+')
+						tag = "%s%s+%s" % (_pos, wordtype, _rest)
+				else:
+					tag = a
 				
 				if gen_only:
 					for c in gen_only:
