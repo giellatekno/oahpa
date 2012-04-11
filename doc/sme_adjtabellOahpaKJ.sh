23ef71f2ab7ada71c9e69955ab744d2dbe50bbb9
leneantonsen <lene.antonsen@uit.no>
2012-04-11 20:54:52 +0000

    La til flere former

--- doc/sme_adjtabellOahpaKJ.sh
+++ doc/sme_adjtabellOahpaKJ.sh
@@ -11,7 +11,7 @@ DATO=`date`
 echo "!!!Tabell over Oahpa-adjektiv med KJ-dialekt" > nudoc/gen/sme_adjOahpatabellKJ.jspwiki
 echo "" >> nudoc/gen/sme_adjOahpatabellKJ.jspwiki
 echo "Testdato: $DATO" >> nudoc/gen/sme_adjOahpatabellKJ.jspwiki
-echo "||  lemma  ||  Attr  ||  Sg Nom || Comp Attr || Comp Sg Nom || Superl Sg Nom" >> nudoc/gen/sme_adjOahpatabellKJ.jspwiki
+echo "||  lemma  ||  Attr  ||  Sg Nom ||  Sg Acc ||  Sg Loc || Sg Ill || Comp Attr || Comp Sg Nom || Superl Sg Nom" >> nudoc/gen/sme_adjOahpatabellKJ.jspwiki
 
 
 # Making the 4 columns
@@ -19,15 +19,21 @@ cat t3|sed 's/$/+A+Attr/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '
 
 cat t3|sed 's/$/+A+Sg+Nom/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8,10,12,14,16|sed 's/.*+A.*/-/'|tr '\t' ',' > tsgnom3
 
+cat t3|sed 's/$/+A+Sg+Acc/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8,10,12,14,16|sed 's/.*+A.*/-/'|tr '\t' ',' > tsgacc3
+
+cat t3|sed 's/$/+A+Sg+Loc/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8,10,12,14,16|sed 's/.*+A.*/-/'|tr '\t' ',' > tsgloc3
+
+cat t3|sed 's/$/+A+Sg+Ill/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8,10,12,14,16|sed 's/.*+A.*/-/'|tr '\t' ',' > tsgill3
+
 cat t3|sed 's/$/+A+Comp+Attr/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8,10,12,14,16|sed 's/.*+A.*/-/'|tr '\t' ',' > tcompattr3
 
 cat t3|sed 's/$/+A+Comp+Sg+Nom/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8,10,12,14,16|sed 's/.*+A.*/-/'|tr '\t' ',' > tcomp3
 
 cat t3|sed 's/$/+A+Superl+Sg+Nom/;'| lookup -q $GTHOME/gt/sme/bin/isme-KJ.restr.fst |tr '\n' '™'|sed 's/™™/£/g;'|tr '£' '\n'|tr '™' '\t'|cut -f2,4,6,8,10,12,14,16|sed 's/.*+A.*/-/'|tr '\t' ',' > tsuperl3
 
-paste -d"|" t3 tattr3 tsgnom3 tcompattr3 tcomp3 tsuperl3  | sed 's/|/ | /g;' | sed 's/^/| /'| sed 's/,/, /g;' | grep '[A-Za-z]' > tadjforms3
+paste -d"|" t3 tattr3 tsgnom3  tsgacc3 tsgloc3 tsgill3 tcompattr3 tcomp3 tsuperl3  | sed 's/|/ | /g;' | sed 's/^/| /'| sed 's/,/, /g;' | grep '[A-Za-z]' > tadjforms3
 
 cat tadjforms3 >> nudoc/gen/sme_adjOahpatabellKJ.jspwiki
 
-rm -f tattr*  tsgnom*  tcomp*   tsuperl* 
+rm -f tattr*  tsg*  tcomp*  tsuperl* 
 
