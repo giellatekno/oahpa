20a741c7e9881f86522d1b1d8bda2e49e6b597ce
Ryan Johnson <ryan.txanson@gmail.com>
2012-05-12 17:46:29 +0000

    include pidfile, for easy killing and restarting

--- univ_oahpa/run_fastcgi.sh
+++ univ_oahpa/run_fastcgi.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-python2.7 manage.py runfcgi method=prefork host=127.0.0.1 port=3032
+﻿#!/bin/bash
+
+PROJDIR="/home/univ_oahpa/univ_oahpa/"
+PIDFILE="$PROJDIR/fastcgi.pid"
+P="python2.7"
+PORT=3032
+
+cd $PROJDIR
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+fi
+
+exec /usr/bin/env - \
+  PYTHONPATH="../python:.." \
+  $P manage.py runfcgi method=prefork host=127.0.0.1 port=$PORT pidfile=$PIDFILE
+
