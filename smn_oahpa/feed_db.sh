74fc76959b6d7fef6f926b913a68672b65e1d83c
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2012-05-17 20:24:54 +0000

    script update

--- smn_oahpa/feed_db.sh
+++ smn_oahpa/feed_db.sh
@@ -1,25 +1,25 @@
 P="python2.6"
-DATA=../sjd
-SJD="$DATA/src"
-RUS="$DATA/russjd"
-$P install.py -f $SJD/a_sjdrus.xml
-$P install.py -f $SJD/adv_sjdrus.xml
-$P install.py -f $SJD/cs_sjdrus.xml
-$P install.py -f $SJD/mwe_sjdrus.xml
-$P install.py -f $SJD/n_sjdrus.xml
-$P install.py -f $SJD/num_sjdrus.xml
-$P install.py -f $SJD/pron_sjdrus.xml
-$P install.py -f $SJD/prop_sjdrus.xml
-$P install.py -f $SJD/v_sjdrus.xml
-$P install.py -f $RUS/a_russjd.xml
-$P install.py -f $RUS/adv_russjd.xml
-$P install.py -f $RUS/cs_russjd.xml
-$P install.py -f $RUS/mwe_russjd.xml
-$P install.py -f $RUS/n_russjd.xml
-$P install.py -f $RUS/num_russjd.xml
-$P install.py -f $RUS/pron_russjd.xml
-$P install.py -f $RUS/prop_russjd.xml
-$P install.py -f $RUS/v_russjd.xml
+DATA=../smn
+SMN="$DATA/src"
+FIN="$DATA/finsmn"
 
-$P install.py --sem $DATA/meta-data/semantical_sets.xml
+for xfile in $(ls $SMN/*.xml)
+do
+  echo "feeding db with: $xfile"
+  $P install.py -f $xfile  
+  echo " "
+  echo "done"
+  echo "   "
+done
+
+for xfile in $(ls $FIN/*.xml)
+do
+  echo "feeding db with: $xfile"
+  $P install.py -f $xfile
+  echo " "
+  echo "done"
+  echo "   "
+done
+
+#$P install.py --sem $DATA/meta-data/semantical_sets.xml
 
