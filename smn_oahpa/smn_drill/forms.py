470209a4d5acb61030c8c281b0b344059d89f532
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2012-09-24 22:14:53 +0000

    still trying to get smn_leksa running

--- smn_oahpa/smn_drill/forms.py
+++ smn_oahpa/smn_drill/forms.py
@@ -913,7 +913,7 @@ class OahpaQuestion(forms.Form):
 # #
 
 class LeksaSettings(OahpaSettings):
-	semtype = forms.ChoiceField(initial='YYY', choices=SEMTYPE_CHOICES)
+	semtype = forms.ChoiceField(initial='BODYPART', choices=SEMTYPE_CHOICES)
 	transtype = forms.ChoiceField(choices=TRANS_CHOICES, widget=forms.Select)
 	# For placename quizz
 	geography = forms.ChoiceField(initial='world', choices=GEOGRAPHY_CHOICES)
@@ -933,7 +933,7 @@ class LeksaSettings(OahpaSettings):
 			'bisyllabic': False,
 			'contracted': False,
 			'source': 'all',
-			'semtype' : 'HUMAN',
+			'semtype' : 'BODYPART',
 			'geography' : 'world',
 			'frequency' : ['common'] # added
 			}
