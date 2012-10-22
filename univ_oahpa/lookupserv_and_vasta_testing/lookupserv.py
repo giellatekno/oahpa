0775e46e2cc7623ea7671dd69775617a81ba4732
Ryan Johnson <ryan.txanson@gmail.com>
2012-10-22 22:42:09 +0000

    quit client if ^D is received

--- univ_oahpa/lookupserv_and_vasta_testing/lookupserv.py
+++ univ_oahpa/lookupserv_and_vasta_testing/lookupserv.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 """
@@ -13,10 +13,9 @@ import sys
 import re
 import os
 import threading
-from threading import Lock
 import time
 
-class Server:
+class Server(object):
     def __init__(self):
         self.host = ''
         self.port = 9000
@@ -25,13 +24,9 @@ class Server:
         self.server = None
         self.threads = []
         
-
-        #fstdir="/Users/saara/gt/sme/bin"
-        #lo="/Users/saara/bin/lookup"
-        fstdir="/opt/smi/sme/bin"
-        lo = "/opt/sami/xerox/c-fsm/ix86-linux2.6-gcc3.4/bin/lookup"
-        #logfile= "/home/saara/foo.log"
-        logfile= "/var/log/lserv.log"
+        fstdir = "/opt/smi/sme/bin"
+        lo = "/usr/bin/lookup"
+        logfile = "/var/log/lserv.log"
         f = open(logfile, 'a')        
 
         fst = fstdir + "/ped-sme.fst"
@@ -45,6 +40,8 @@ class Server:
     def open_socket(self):
         try:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Reuse socket in case of server timeout while in TIME_WAIT
+            self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server.bind((self.host,self.port))
             self.server.listen(5)
         except socket.error, (value,message):
@@ -73,7 +70,7 @@ class Server:
             c.join()
                             
 class Client(threading.Thread):
-	
+    
     def __init__(self,(client,address),look,lock):
         threading.Thread.__init__(self)
         self.client = client
@@ -81,39 +78,57 @@ class Client(threading.Thread):
         self.size = 1024
         self.look=look
         self.lock=lock
-        #self.lookup2cg = " | /Users/saara/gt/script/lookup2cg"
         self.lookup2cg = " | /usr/local/bin/lookup2cg"
+       
+    # NOTE: Just in case you need to see data from the client only
+    # def clientLog(self, msg):
+    #     with open('/var/log/lserv.client.log', 'a') as LOG:
+    #         LOG.writelines([msg + '\n'])
 
     def run(self):
         running = 1
         while running:
             data = self.client.recv(self.size)
+            # NOTE: for testing, uncomment. Log is destroyed with each new
+            # client though
+            # self.clientLog(repr(data))
             data = data.strip()
             if not data:
                 self.client.close()
                 running = 0
                 continue
+            elif data == '\x04':
+                # Client sends ^D, close, rather than waiting for more input
+                # TODO: handle ^C? 
+                self.client.close()
+                running = 0
+                continue
 
             data2=data
-			# clean the data for command line
-            c = [";","<",">","*","|","`","&","$","!","#","(",")","[","]","{","}",":","@","\"","\r"]
+            # clean the data for command line
+            c = """;<>*|`&$!#()[]{}:@\"\r""".split()
             for a in c:
                 data = data.replace(a,'')
 
-			# Take out non-breaking space. Try to avoid errors.
+            # Take out non-breaking space. Try to avoid errors.
             try:
-				data2 = data.decode('utf-8')
-				data2 = data2.replace(unichr(160),"")
-				data = data2.encode('utf8')
-            except:
+                data2 = data.decode('utf-8')
+                data2 = data2.replace(unichr(160),"")
+                data = data2.encode('utf8')
+            except UnicodeDecodeError:
+                # If something cannot be decoded, better to quit.
+                self.client.send("error")
+                self.client.close()
+            except Exception, e:
                 #data = data.encode('utf-8')
                 #f.write("Not utf-8: " + data)
                 self.client.send("error")
+                running = 0
                 continue
 
                 #data = re.sub(r'[^\wáŋčžšđŧÁŊĐÁŠŦŽČ_- åäöÅÄÖæøÆØ]+', '',data, re.U)
-				
-			# if the data contained only special characters, return the original data.
+                
+            # if the data contained only special characters, return the original data.
             if not data:
                 self.client.send("error")
                 continue
@@ -121,19 +136,19 @@ class Client(threading.Thread):
             # quit with q
             if ( data.strip() == 'q' or data == 'Q'):
                 self.client.close()
-                running = 0				
+                running = 0             
                 continue
-			
+            
             data = data.strip()+ "\n"
 
-            #f = open('/var/log/lserv.log', 'a')			
+            #f = open('/var/log/lserv.log', 'a')            
 
             self.lock.acquire()
             #f.write("input: "+ data)
             #f.write("\n")
-            self.look.sendline(data)			
+            self.look.sendline(data)            
             index = self.look.expect(['\r?\n\r?\n','ERROR',pexpect.EOF, pexpect.TIMEOUT])
-			
+            
             #If there is an error, then restart the lookup process
             if index ==1 or index==2 or index==3:
                 try:
@@ -152,7 +167,7 @@ class Client(threading.Thread):
                 #f.write("\n")
 
                 #f.close()
-				
+                
                 # hack for removing the stderr from lookup 0%>>>>>>100% ...
                 result = result.replace('100%','')
                 result = result.replace('0%','')
@@ -180,3 +195,6 @@ class Client(threading.Thread):
 if __name__ == "__main__":
     s = Server()
     s.run()
+
+
+# vim: set ts=4 sw=4 tw=0 syntax=python expandtab:
