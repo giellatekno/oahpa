5304e366beeae741e241f46ee793a86093c2c07c
Ryan Johnson <ryan.txanson@gmail.com>
2013-02-09 01:48:55 +0000

    updating bookmarklet code to use sanit

--- kursadict/bookmarklet_code.py
+++ kursadict/bookmarklet_code.py
@@ -33,7 +33,7 @@ bookmarklet = """
 # This is the end product of minifying the above, but note that some of
 # this code will need to be urlencoded if support for older browsers is
 # desired.
-bookmarklet_minified = """(function(){var e="http://digitesting.oahpa.no";var t=document.createElement("link");t.href=e+"/static/css/jquery.neahttadigisanit.css";t.rel="stylesheet";document.head.appendChild(t);var n=document.createElement("script");n.type="text/javascript";n.src=e+"/static/js/bookmarklet.min.js";window.NDS_API_HOST=e;document.body.appendChild(n)})()"""
+bookmarklet_minified = """(function(){var e="http://sanit.oahpa.no";var t=document.createElement("link");t.href=e+"/static/css/jquery.neahttadigisanit.css";t.rel="stylesheet";document.head.appendChild(t);var n=document.createElement("script");n.type="text/javascript";n.src=e+"/static/js/bookmarklet.min.js";window.NDS_API_HOST=e;document.body.appendChild(n)})()"""
 
 # This part should be url encoded and inserted within the following
 # (function()HERE)()
@@ -42,5 +42,5 @@ bookmarklet_minified = """(function(){var e="http://digitesting.oahpa.no";var t=
 # browsers, but just in case...
 # http://meyerweb.com/eric/tools/dencoder/
 
-bookmarklet_escaped = """(function()%7Bvar%20e%3D%22http%3A%2F%2Fdigitesting.oahpa.no%22%3Bvar%20t%3Ddocument.createElement(%22link%22)%3Bt.href%3De%2B%22%2Fstatic%2Fcss%2Fjquery.neahttadigisanit.css%22%3Bt.rel%3D%22stylesheet%22%3Bdocument.head.appendChild(t)%3Bvar%20n%3Ddocument.createElement(%22script%22)%3Bn.type%3D%22text%2Fjavascript%22%3Bn.src%3De%2B%22%2Fstatic%2Fjs%2Fbookmarklet.min.js%22%3Bwindow.NDS_API_HOST%3De%3Bdocument.body.appendChild(n)%7D)()"""
+bookmarklet_escaped = """(function()%7Bvar%20e%3D%22http%3A%2F%2Fsanit.oahpa.no%22%3Bvar%20t%3Ddocument.createElement(%22link%22)%3Bt.href%3De%2B%22%2Fstatic%2Fcss%2Fjquery.neahttadigisanit.css%22%3Bt.rel%3D%22stylesheet%22%3Bdocument.head.appendChild(t)%3Bvar%20n%3Ddocument.createElement(%22script%22)%3Bn.type%3D%22text%2Fjavascript%22%3Bn.src%3De%2B%22%2Fstatic%2Fjs%2Fbookmarklet.min.js%22%3Bwindow.NDS_API_HOST%3De%3Bdocument.body.appendChild(n)%7D)()"""
 
