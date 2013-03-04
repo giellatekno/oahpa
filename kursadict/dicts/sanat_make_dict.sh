bc08d05b46310a7867437219695ca5662522d9f2
Ryan Johnson <ryan.txanson@gmail.com>
2013-03-04 02:27:37 +0000

    oops, i was right, getting lines crossed in my own notes, yay

--- kursadict/dicts/sanat_make_dict.sh
+++ kursadict/dicts/sanat_make_dict.sh
@@ -1,11 +1,19 @@
 # Script complains if directory has lexc files in it, so copy
 echo "Compiling olo-fin.all.xml"
-mkdir `pwd`/olo
+mkdir olo
 cp $GTHOME/langs/olo/src/morphology/stems/*.xml `pwd`/olo/
 java -Xmx2048m -cp ~/lib/saxon9.jar -Dfile.encoding=UTF8 net.sf.saxon.Transform \
     -it:main $GTHOME/words/dicts/scripts/collect-dict-parts.xsl \
     inDir=`pwd`/olo/ > olo-fin.all.xml
-rm -rf `pwd`/olo
+rm -rf olo
+
+echo "Compiling liv-fin.all.xml"
+mkdir liv
+cp $GTHOME/langs/liv/src/morphology/stems/*.xml `pwd`/liv/
+java -Xmx2048m -cp ~/lib/saxon9.jar -Dfile.encoding=UTF8 net.sf.saxon.Transform \
+    -it:main $GTHOME/words/dicts/scripts/collect-dict-parts.xsl \
+    inDir=`pwd`/liv/ > liv-fin.all.xml
+rm -rf liv
 
 
 echo "Done"
--- kursadict/dicts/valks_make_dict.sh
+++ kursadict/dicts/valks_make_dict.sh
@@ -1,7 +1,10 @@
 # Script complains if directory has lexc files in it, so copy
-echo "Compiling kom2x.all.xml"
+echo "Compiling myv / Erzya Mordvin"
+mkdir myv
+cp $GTHOME/langs/myv/src/morphology/stems/*.xml `pwd`/myv/
 java -Xmx2048m -cp ~/lib/saxon9.jar -Dfile.encoding=UTF8 net.sf.saxon.Transform \
     -it:main $GTHOME/words/dicts/scripts/collect-dict-parts.xsl \
-    inDir=$GTHOME/words/dicts/kom2X/src/ > kom2x.all.xml
+    inDir=`pwd`/myv/ > myv-all.xml
+rm -rf myv
 
 echo "Done"
