b478fe44583f2e17b4730b20029c7cd077223ea4
Ryan Johnson <ryan.txanson@gmail.com>
2013-03-15 01:26:48 +0000

    mdf

--- kursadict/dicts/valks_cp_analyzers.sh
+++ kursadict/dicts/valks_cp_analyzers.sh
@@ -7,6 +7,10 @@ sudo mkdir -p /opt/smi/myv/bin/
 sudo cp $GTHOME/langs/myv/src/analyser-gt-desc.xfst  /opt/smi/myv/bin/myv.fst
 sudo cp $GTHOME/langs/myv/src/generator-gt-norm.xfst /opt/smi/myv/bin/imyv.fst
 
+sudo mkdir -p /opt/smi/mdf/bin/
+sudo cp $GTHOME/langs/mdf/src/analyser-gt-desc.xfst  /opt/smi/mdf/bin/mdf.fst
+sudo cp $GTHOME/langs/mdf/src/generator-gt-norm.xfst /opt/smi/mdf/bin/imdf.fst
+
 # sudo mkdir -p /opt/smi/rus/bin/
 # sudo cp $GTHOME/langs/rus/src/analyser-gt-desc.xfst  /opt/smi/rus/bin/rus.fst
 # sudo cp $GTHOME/langs/rus/src/generator-gt-norm.xfst /opt/smi/rus/bin/irus.fst
--- kursadict/dicts/valks_make_dict.sh
+++ kursadict/dicts/valks_make_dict.sh
@@ -8,4 +8,12 @@ java -Xmx2048m -cp ~/lib/saxon9.jar -Dfile.encoding=UTF8 net.sf.saxon.Transform 
     inDir=`pwd`/myv/ > myv-all.xml
 rm -rf myv
 
+echo "Compiling mdf / Moksha"
+mkdir mdf
+cp $GTHOME/langs/mdf/src/morphology/stems/*.xml mdf/
+java -Xmx2048m -cp ~/lib/saxon9.jar -Dfile.encoding=UTF8 net.sf.saxon.Transform \
+    -it:main $GTHOME/words/dicts/scripts/collect-dict-parts.xsl \
+    inDir=`pwd`/mdf/ > mdf-all.xml
+rm -rf mdf
+
 echo "Done"
