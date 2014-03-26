af211e24e6abcf1f8d9659877665a77866de24ab
Ryan Johnson <ryan.txanson@gmail.com>
2014-03-26 22:04:41 +0000

    missing file

---
+++ univ_oahpa/courses/templatetags/goal_filters.py
@@ -0,0 +1,17 @@
+﻿from django import template
+
+register = template.Library()
+
+@register.filter(name='complete_for')
+def complete_for(a, user):
+    return a.user_completed(user)
+
+@register.filter(name='goal_instances_for')
+def goal_instances_for(instances, username):
+    return instances.filter(user__username=username)
+
+@register.filter(name='format_percent')
+def format_percent(_float):
+    _perc = "%s" % (_float*100).to_eng_string()
+    return _perc + '%'
+
