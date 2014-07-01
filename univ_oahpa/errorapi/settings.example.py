fb67838f33ff67a1bbbefe6f5cdd8965ee5bf8f7
Ryan Johnson <ryan.txanson@gmail.com>
2014-07-01 23:05:49 +0000

    add example settings

---
+++ univ_oahpa/errorapi/settings.example.py
@@ -0,0 +1,13 @@
+﻿
+# Imagine this is in settings.py, thus all paths should be relative to
+# that.
+
+_join_path = lambda x: os.path.join(os.getcwd(), x)
+
+ERROR_FST_SETTINGS = {
+	'lookup_tool': '/usr/local/bin/lookup -flags mbTT',
+	'fst_path': '/opt/smi/sme/bin/ped-errortag-sme.fst',
+	'error_message_files': {
+		'nob': _join_path('../meta/morfaerrorfstmessages.xml'),
+    }
+}
