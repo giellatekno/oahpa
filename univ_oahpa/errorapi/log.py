ca0db203a5aca32a60644f3bfd6761be0e902ecc
Ryan Johnson <ryan.txanson@gmail.com>
2014-07-02 19:36:20 +0000

    change means for getting error path, for fastcgi implmentation

--- univ_oahpa/errorapi/log.py
+++ univ_oahpa/errorapi/log.py
@@ -1,6 +1,12 @@
 ﻿import logging
 import os
 
+from django.conf import settings
+
+ERROR_FST_SETTINGS = settings.ERROR_FST_SETTINGS
+
+error_log_path = ERROR_FST_SETTINGS.get('error_log_path')
+
 __all__ = [
     'ERROR_FST_LOG',
 ]
@@ -8,8 +14,7 @@ __all__ = [
 # Logging globals
 
 ERROR_FST_FORMATTER = logging.Formatter('%(asctime)-15s - %(message)s')
-ERROR_FST_LOG_FILE_PATH = os.path.join(os.getcwd(),
-                                       'error_api_log.txt')
+ERROR_FST_LOG_FILE_PATH = error_log_path
 
 ERROR_FST_FILE = logging.FileHandler(ERROR_FST_LOG_FILE_PATH)
 ERROR_FST_FILE.setFormatter(ERROR_FST_FORMATTER)
--- univ_oahpa/errorapi/settings.example.py
+++ univ_oahpa/errorapi/settings.example.py
@@ -7,6 +7,7 @@ _join_path = lambda x: os.path.join(os.getcwd(), x)
 ERROR_FST_SETTINGS = {
 	'lookup_tool': '/usr/local/bin/lookup -flags mbTT',
 	'fst_path': '/opt/smi/sme/bin/ped-errortag-sme.fst',
+	'error_log_path': _join_path('error_fst_log.txt'),
 	'error_message_files': {
 		'nob': _join_path('../meta/morfaerrorfstmessages.xml'),
     }
