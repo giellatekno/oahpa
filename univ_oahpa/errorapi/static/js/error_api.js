a0b4314a516d5b593612d717f61dffa8b736ddf6
Ryan Johnson <ryan.txanson@gmail.com>
2014-08-05 21:35:59 +0000

    popovers

--- univ_oahpa/errorapi/static/js/error_api.js
+++ univ_oahpa/errorapi/static/js/error_api.js
@@ -9,11 +9,12 @@ var ErrorAPI = angular.module('ErrorAPI', ['ngCookies', 'angular-loading-bar']).
         $httpProvider.defaults.withCredentials = true;
     });
 
-
-
 ErrorAPI.controller('ErrorRequester', function($scope, $http, $element, $cookies) {
+
     function handle_api_response (data) {
+
         $scope.response = data;
+
         if(data.messages.length > 0) {
             $scope.messages = data.messages;
             $scope.analyzer = data.fst[0];
@@ -23,6 +24,7 @@ ErrorAPI.controller('ErrorRequester', function($scope, $http, $element, $cookies
             $scope.analyzer = false
             $scope.no_errors = true;
         }
+
     }
 
     var feedback_url = $element.attr('data-lookup-url') ;
@@ -35,7 +37,7 @@ ErrorAPI.controller('ErrorRequester', function($scope, $http, $element, $cookies
         var css_selector = "a[data-error-fst]";
     }
 
-    $element.find("form").bind('submit', function($event){
+    $element.find("form").bind('submit', function ($event) {
 
         var feedback_data = {
             'lookup': $scope.text_input,
@@ -45,6 +47,7 @@ ErrorAPI.controller('ErrorRequester', function($scope, $http, $element, $cookies
         if ($scope.task) {
             feedback_data['task'] = $scope.task;
         }
+
         if ($scope.lemma) {
             feedback_data['intended_lemma'] = $scope.lemma;
         }
@@ -61,11 +64,48 @@ ErrorAPI.controller('ErrorRequester', function($scope, $http, $element, $cookies
         
     });
 
-    $element.find(css_selector).bind('click', function($event){
+    $element.find(css_selector).bind('click', function ($event) {
 
         $event.preventDefault();
 
         var feedback_link = $($event.target);
+
+        function handle_api_feedback_popover (data) {
+            handle_api_response(data);
+
+            $('.popover').remove();
+
+            if ($scope.messages) {
+
+                // Collect feedback messages.
+                // Attempted to do this by injecting an angular template, but
+                // it was too tricky.
+                var message_body_snippet = "<ul class='errorapi_messages'>";
+
+                for (_i = 0, _len = $scope.messages.length; _i < _len; _i++) {
+                    msg = $scope.messages[_i];
+                    console.log(msg);
+
+                    for (_j = 0, _lenz = msg.message.length; _j < _lenz; _j++) {
+                        m = msg.message[_j];
+                        message_body_snippet += "<li>" + m.string + "</li>";
+                    }
+
+                }
+                message_body_snippet += "</ul>";
+
+            } else {
+                // TODO: localize
+                message_body_snippet = "<p>No errors :)</p>";
+            }
+
+            feedback_link.popover({
+                title: 'Feedback',
+                content: message_body_snippet,
+                html: true,
+            });
+            feedback_link.popover('show');
+        }
 
         // Prepare request
         var form  = $(feedback_link).html();
@@ -85,8 +125,6 @@ ErrorAPI.controller('ErrorRequester', function($scope, $http, $element, $cookies
             feedback_data['intended_lemma'] = lemma;
         }
 
-        console.log([form, task, lemma]);
-    
         var config = {
             withCredentials: true,
             headers: { 'Content-Type': 'application/json' }
@@ -95,8 +133,8 @@ ErrorAPI.controller('ErrorRequester', function($scope, $http, $element, $cookies
         feedback_data = JSON.stringify(feedback_data);
 
         $http.post(feedback_url, feedback_data, config)
-             .success(handle_api_response);
+             .success(handle_api_feedback_popover);
 
-    });
+    }); // click event
 
 });
