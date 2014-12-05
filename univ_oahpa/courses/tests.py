7f6127c39cc31bb8d8bfc28ce02496a2b076dc92
Ryan Johnson <ryan.txanson@gmail.com>
2014-12-05 21:41:01 +0000

    moving test notes

--- univ_oahpa/courses/tests.py
+++ univ_oahpa/courses/tests.py
@@ -9,6 +9,25 @@ from django.test import TestCase
 
 from rest_framework.test import APIRequestFactory
 
+# TODO: create tests for the following procedure
+
+# log in 
+
+    # http --session=courses_test get http://localhost:8000/davvi/courses/standard_login/ | grep csrfmiddlewaretoken | head -n 1 | grep -o "value='\(.*\)'" | sed 's/value=//' | tr -d "'" > token.tmp
+    # http -f --session=courses_test POST http://localhost:8000/davvi/courses/standard_login/ username=asdf password=asdf csrfmiddlewaretoken=`cat token.tmp`
+
+
+# list goals for referer
+
+    # http --session=courses_test GET http://localhost:8000/davvi/courses/api/submission/ Referer:"localhost"
+
+# Log the first action
+
+    # http --session=courses_test --json POST http://localhost:8000/davvi/courses/api/submission/ user_input=blahblah correct=asdf,bbq iscorrect=True task_id=91
+
+
+    # http --verbose --session=courses_test --json POST http://localhost:8000/davvi/courses/api/submission/ Referer:localhost X-CSRFToken:`cat token.tmp` user_input=blahblah correct=asdf,bbq iscorrect=True task_id=91
+
 
 
 class SimpleTest(TestCase):
