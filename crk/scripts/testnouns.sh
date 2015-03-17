af1cbe93229391b5fc57df2da1e49cd019ebd1bd
leneantonsen <lene.antonsen@uit.no>
2015-03-17 12:36:14 +0000

    The test gives and opens two dokuments: testresult.txt with lemmas which are not generated and how the dependent nouns are generated for Px1Sg. generation_allnouns.txt with all generation for independent nouns, for checking overgeneration.

--- crk/scripts/testnouns.sh
+++ crk/scripts/testnouns.sh
@@ -1,14 +1,18 @@
-grep '<l ' src/N_crk.xml | grep -v '"none"' |grep AN | tr '<' '>' | cut -d '>' -f3 | grep -v '^-' |sed 's/$/+N+AN+Sg/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst > gennouns.txt
+#sh scripts/testnouns.sh in ped/crk directory
 
-grep '<l ' src/N_crk.xml | grep -v '"none"' |grep IN | tr '<' '>' | cut -d '>' -f3 |sed 's/$/+N+IN+Sg/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst >> gennouns.txt
+grep '<l ' src/N_crk.xml | grep -v '"none"' |grep AN | tr '<' '>' | cut -d '>' -f3 | grep -v '^-' |sed 's/$/+N+AN+Sg/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst > generation_allnouns.txt
 
-cat gennouns.txt | cut -f1 | cut -d '+' -f1 | sort -u > lemmas
-cat gennouns.txt | cut -f2 | sort -u > generated
-comm -23 lemmas generated > testresult.txt
+grep '<l ' src/N_crk.xml | grep -v '"none"' |grep IN | tr '<' '>' | cut -d '>' -f3 |sed 's/$/+N+IN+Sg/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst >> generation_allnouns.txt
+
+cat generation_allnouns.txt | cut -f1 | cut -d '+' -f1 | sort -u > lemmas
+cat generation_allnouns.txt | cut -f2 | sort -u > generated
+echo "Not generated lemma(s):" > testresult.txt
+comm -23 lemmas generated >> testresult.txt
 echo "" >> testresult.txt
-echo "-------" >> testresult.txt
+echo "Generating -lemmas:" >> testresult.txt
 echo "" >> testresult.txt
 grep '<l ' src/N_crk.xml | grep -v '"none"' |grep 'AN.*>-' | tr '<' '>' | cut -d '>' -f3 |sed 's/$/+N+AN+Sg+Px1Sg/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst >> testresult.txt
 
 open testresult.txt
+open generation_allnouns.txt
 
