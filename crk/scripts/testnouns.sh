5ede1fdbe6f7d2a09bcd3757ee5e9f7a4d459dc9
leneantonsen <lene.antonsen@uit.no>
2015-03-17 08:09:27 +0000

    Improvements

--- crk/scripts/testnouns.sh
+++ crk/scripts/testnouns.sh
@@ -2,8 +2,13 @@ grep '<l ' src/N_crk.xml | grep -v '"none"' |grep AN | tr '<' '>' | cut -d '>' -
 
 grep '<l ' src/N_crk.xml | grep -v '"none"' |grep IN | tr '<' '>' | cut -d '>' -f3 |sed 's/$/+N+IN+Sg/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst >> gennouns.txt
 
-grep '<l ' src/N_crk.xml | grep -v '"none"' |grep 'AN.*>-' | tr '<' '>' | cut -d '>' -f3 |sed 's/$/+N+AN+Sg+PxSg1/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst >> gennouns.txt
-
 cat gennouns.txt | cut -f1 | cut -d '+' -f1 | sort -u > lemmas
 cat gennouns.txt | cut -f2 | sort -u > generated
-comm -23 lemmas generated | see
+comm -23 lemmas generated > testresult.txt
+echo "" >> testresult.txt
+echo "-------" >> testresult.txt
+echo "" >> testresult.txt
+grep '<l ' src/N_crk.xml | grep -v '"none"' |grep 'AN.*>-' | tr '<' '>' | cut -d '>' -f3 |sed 's/$/+N+AN+Sg+Px1Sg/' |$LOOKUP $GTHOME/langs/crk/src/generator-gt-desc.xfst >> testresult.txt
+
+open testresult.txt
+
