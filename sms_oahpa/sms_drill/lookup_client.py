6022239337da6eeac0988b5c46f8cb0dcca56b9e
Ryan Johnson <ryan.txanson@gmail.com>
2015-07-26 20:45:15 +0000

    turn lookups into db objects

--- sms_oahpa/sms_drill/lookup_client.py
+++ sms_oahpa/sms_drill/lookup_client.py
@@ -7,7 +7,44 @@ and uses python's default telnetlib
 
 from .models import Tag
 
-def lookup(tool_name, input_string, host='127.0.0.1', port=9001, timeout=2, until='\n\n'):
+def init_logger():
+
+    import logging
+    from logging.handlers import WatchedFileHandler
+    FORMAT = '%(message)s'
+    formatter = logging.Formatter(fmt=FORMAT)
+
+    handler = WatchedFileHandler('sms_lookup_client.log', mode='a')
+    handler.setFormatter(formatter)
+
+
+    client_log = logging.getLogger('lookup_client')
+    client_log.setLevel(logging.INFO)
+    client_log.addHandler(handler)
+
+    return client_log
+
+
+client_log = init_logger()
+
+def log(*args):
+
+    a_str = ''
+    if len(args) > 0:
+        for a in args:
+            try:
+                a_str += ' ' + a
+            except:
+                a_str += ' ' +repr(a)
+
+    try:
+        s = a_str.encode('utf-8')
+    except:
+        s = a_str
+    client_log.info(s)
+
+
+def send_lookup(tool_name, input_string, host='127.0.0.1', port=9001, timeout=2, until='\n\n'):
     import telnetlib
     import sys
 
@@ -34,34 +71,56 @@ def lookup(tool_name, input_string, host='127.0.0.1', port=9001, timeout=2, unti
     
     return True, d
 
-def parse_lookup(lookup_string):
+def lookup(*args, **kwargs):
     """
-        Clean XFST lookup text into
+        Clean XFST lookup text, perform lookups in the database for
+        corresponding lemmas and tags.
 
-        [('keenaa', ['keen+V+1Sg+Ind+Pres', 'keen+V+3SgM+Ind+Pres']),
-         ('keentaa', ['keen+V+2Sg+Ind+Pres', 'keen+V+3SgF+Ind+Pres'])]
+        Returns (success, lookup_list):
+            success - boolean
+            lookup_list - any lookups present
 
-
-        Borrowed from NDS
+        Success is false if there were no lookups.
 
     """
+
+    from .models import Tag, Word
+
+    success, lookup_string = send_lookup(*args, **kwargs)
 
     def clean_analysis(analysis):
+        """ Convert analyses to db objects.
+
+            TODO: if lemma exists, but isn't in db, what then? We don't
+            care about these lemmas, probably, because we only care
+            about the one the DB generated
+
+        """
         lemma, _, tag = analysis.partition('+')
+        log(lemma)
+
+        if analysis.endswith('+?'):
+            return (False, False)
+
         try:
-            t = Tag.objects.get(string=tag)
-        except Tag.DoesNotExist:
-            print 'no <Tag> for: ' + tag
-            return False, False
-        return (lemma, t)
+            l = Word.objects.get(lemma=lemma)
+        except Word.DoesNotExist:
+            return (False, False)
+
+        t, created = Tag.objects.get_or_create(string=tag)
+
+        if created:
+            log('>> lookup tool returned tag not in DB, created: ' + repr(t))
+        return (l, t)
 
     def split_a_line(analysis_line):
         wf, _, tag = analysis_line.partition('\t')
+
         l, t_obj = clean_analysis(tag)
-        if l:
+        if l and t_obj:
             return (l, t_obj)
         else:
-            return (wf, tag)
+            return (False, False)
 
     analysis_chunks = [a for a in lookup_string.split('\n\n') if a.strip()]
 
@@ -81,4 +140,14 @@ def parse_lookup(lookup_string):
 
         cleaned.append(append_)
 
-    return cleaned
+    # TODO: filter out null analyses in cleaned, if none exist, then
+    # success = False
+
+    log("--")
+    log("success    ", success)
+    log("analysis    ", cleaned)
+
+    if success:
+        return success, cleaned
+    else:
+        return False, []
