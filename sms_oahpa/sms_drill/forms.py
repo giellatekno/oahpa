785206b8be1e597a4ac77758ebbc4c0b20bf2217
Jack Rueter <rueter.jack@gmail.com>
2015-08-09 05:55:07 +0000

    The +Indef tag has been removed from noun paradigm.

--- sms_oahpa/sms_drill/forms.py
+++ sms_oahpa/sms_drill/forms.py
@@ -84,17 +84,6 @@ CASE_CHOICES = (
 # For now this is just a part of a test, used in game.Game.get_db_info_new
 # I wanted a very general way to specify question/answers.
 
-#NOUN_QUESTION_ANSWER = {
-#	# gametype			question		answer
-#	'NOMPL': [('N+Sg+Nom+Indef', 'N+Pl+Nom+Indef')],
-#	'N-ACC': [('N+NumberN+Nom+Indef', 'N+NumberN+Acc+Indef')],
-#	'N-ILL': [('N+NumberN+Nom+Indef', 'N+NumberN+Ill+Indef')],
-#	'N-LOC': [('N+NumberN+Nom+Indef', 'N+NumberN+Loc+Indef')],
-#	'N-COM': [('N+NumberN+Nom+Indef', 'N+NumberN+Com+Indef')],
-#	'N-GEN': [('N+NumberN+Nom+Indef', 'N+NumberN+Gen+Indef')],
-#	'N-ESS': [('N+NumberN+Nom+Indef', 'N+Ess+Indef')],
-#}
-
 NOUN_QUESTION_ANSWER = {
         # gametype                      question                answer         
         'NOMPL': [('N+Sg+Nom', 'N+Pl+Nom')],
