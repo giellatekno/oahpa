0f332cc9d1e6bc80c6abc2f5afaab15e31fb6b0f
Ryan Johnson <ryan.txanson@gmail.com>
2016-05-24 16:37:14 +0000

    add verb questions to validation

--- crk_oahpa/validate_xml.sh
+++ crk_oahpa/validate_xml.sh
@@ -1,2 +1,2 @@
-ls -c1 ../crk/src/*.xml ../crk/meta/*.xml | xargs xmllint --format 1>err.tmp
+ls -c1 ../crk/src/*.xml ../crk/meta/*.xml ../crk/meta/verb_questions/*.xml | xargs xmllint --format 1>err.tmp
 rm err.tmp
