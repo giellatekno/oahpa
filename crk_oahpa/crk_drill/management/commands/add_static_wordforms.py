db2e7d868f70a86bfd89823caec517812eca26e7
Ryan Johnson <ryan.txanson@gmail.com>
2016-06-03 00:25:32 +0000

    extra install command for updating words in db with new forms from a static list

---
+++ crk_oahpa/crk_drill/management/commands/add_static_wordforms.py
@@ -0,0 +1,98 @@
+from django.core.management.base import BaseCommand, CommandError
+
+# from_yaml(cls, loader, node)
+
+from optparse import make_option
+
+import sys
+
+from crk_drill.models import Form
+
+def install_file(filename):
+    from collections import defaultdict
+    words_to_install = defaultdict(list)
+    with open(filename, 'r') as F:
+        for l in F.readlines():
+            tag, _, form = l.strip().partition('\t')
+            lemma, _, tag = tag.partition('+')
+            if lemma in words_to_install:
+                if tag in words_to_install[lemma]:
+                    words_to_install[lemma][tag].append(form)
+                else:
+                    words_to_install[lemma] = {tag: [form]}
+            else:
+                words_to_install[lemma] = {tag: [form]}
+
+    from itertools import izip_longest
+
+    for lemma, forms in words_to_install.iteritems():
+        print 'lemma: ', lemma
+        for tag, wfs in forms.iteritems():
+            fs = Form.objects.filter(word__lemma=lemma, tag__string=tag)
+            print '  ', tag
+            last_db = False
+            for new_form, db_form in izip_longest(wfs, fs, fillvalue=False):
+                if db_form:
+                    print '    ', "updating ", db_form, " with ", new_form
+                    db_form.fullform = new_form
+                    db_form.save()
+                else:
+                    print '    ', "creating new form for ", db_form, " with ", new_form
+                    new = Form.objects.create(word=last_db.word,
+                                        tag=last_db.tag,
+                                        fullform=new_form,)
+                    for d in last_db.dialects.all():
+                        new.dialects.add(d)
+                    for fb in last_db.feedback.all():
+                        new.feedback.add(fb)
+                    new.save()
+
+                last_db = db_form
+
+
+
+# # # 
+# 
+#  Command class
+#
+# # #
+
+def mergetags(tfilter=False):
+    if tfilter:
+        qset = Tag.objects.filter(string=tfilter)
+    else:
+        qset = Tag.objects.all()
+    
+    strings = qset.values_list('string', flat=True)
+    strings = list(set(strings))
+
+    print 'Merging:'
+    for string in strings:
+        tag = Tag.objects.filter(string=string)
+
+        if tag.count() > 1:
+            print 'Merging conflict in %s' % tag[0].string
+            merge(tag)
+    
+
+class Command(BaseCommand):
+    args = '--tagelement'
+    help = """
+    Strips tags of an element and then merges them all.
+    """
+    option_list = BaseCommand.option_list + (
+        make_option("-f", "--filename", dest="filename", default=False,
+                          help="Static file to read from"),
+        # make_option("-d", "--dryrun", dest="dryrun", default="True",
+        #                   help="List tags matching element instead of merging"),
+
+        # TODO: question iterations count
+    )
+
+    def handle(self, *args, **options):
+        import sys, os
+
+        filename = options['filename']
+
+        install_file(filename=filename)
+
