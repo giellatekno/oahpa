c8e76ef0c88da2ef8e44c9194ea086a6fcdc2349
Tiina Puolakainen <>
2016-06-09 05:55:23 +0000

    Fix

--- rus_oahpa/wsgi.py
+++ rus_oahpa/wsgi.py
@@ -3,6 +3,5 @@ import os, sys
 from os import environ
 from django.core.wsgi import get_wsgi_application
     
-sys.path.append(' /Users/tiina/main/ped/rus_oahpa')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 application = get_wsgi_application()
