aec880d0fd0af0890025e19270779c986daa5ff7
Heli Uibo <heli1401@gmail.com>
2016-10-04 09:41:33 +0000

    longer field for audio links

--- vro_oahpa/vro_drill/models.py
+++ vro_oahpa/vro_drill/models.py
@@ -469,7 +469,7 @@ class Word(models.Model):
 	gen2 = models.BooleanField(default=False) # indicates if the word has Genitive2 or not
 	reflexive = models.NullBooleanField(blank=True)
 	inflection_class = models.CharField(max_length=20) # Zaliznyak's number class
-	audio = models.CharField(max_length=50) # audio file name
+	audio = models.CharField(max_length=70) # audio file name
 	zaliznjak = models.CharField(max_length=20)
 	wordclass = models.CharField(max_length=8)
 	# valency = models.CharField(max_length=10)
