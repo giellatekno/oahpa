f87ed5767954f05acaa280a93606de8dbdd50841
Heli Uibo <heli1401@gmail.com>
2016-11-14 13:14:31 +0000

    Some corrections in the user interface.

--- vro_oahpa/vro_drill/forms.py
+++ vro_oahpa/vro_drill/forms.py
@@ -1141,9 +1141,9 @@ class LeksaQuestion(OahpaQuestion):
 							preferred=preferred)
 
 		if self.translang == "vro":
-            for transl_word in translations:
-                transl_obj = Word.objects.get(lemma=transl_word)
-                self.transl_audio = transl_obj.audio
+			for transl_word in translations:
+				transl_obj = Word.objects.get(lemma=transl_word)
+				self.transl_audio = transl_obj.audio
 		
 		self.is_correct("leksa", self.lemma)
 		# set correct and error values
