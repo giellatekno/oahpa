2eea9152195b91012a6b32c09aaf25b7d9f0db1b
Chiara Argese <argese.chiara@gmail.com>
2018-11-15 10:36:19 +0000

    add conf folder to sjd oahpa

---
+++ sjd_oahpa_project/sjd_oahpa/conf/context_processors.py
@@ -0,0 +1,39 @@
+import sjd_oahpa.settings
+
+try:
+	default = sjd_oahpa.settings.DEFAULT_DIALECT
+except:
+	default = 'GG'
+
+def dialect(request):
+    if not request.session.get('dialect'):
+    	request.session['dialect'] = default
+    return {'dialect': request.session.get('dialect')}
+
+def site_root(request):
+	return {'URL_PREFIX': sjd_oahpa.settings.URL_PREFIX}
+
+def grammarlinks(request):
+	from sjd_drill.models import Grammarlinks
+	from conf.tools import switch_language_code
+	
+	default_lang = 'nob'
+	
+	try:
+		lang = switch_language_code(request.session.get('django_language'))
+	except:
+		lang = default_lang
+	
+	if lang == 'fin':
+		lang = 'sme'  # If interface language is Finnish then the grammar explanations will be in North Sami.
+	links = Grammarlinks.objects.filter(language=lang).order_by('name')
+	
+	if links.count() == 0:
+		lang = default_lang
+		links = Grammarlinks.objects.filter(language=lang).order_by('name')
+	
+	if links.count () > 0:
+		return {'grammarlinks': links, 'cache_language': lang}
+	else:
+		return {'grammarlinks': False, 'cache_language': 'x'}
+
