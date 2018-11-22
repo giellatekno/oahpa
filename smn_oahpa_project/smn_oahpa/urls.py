a5480685ddbc5e6bda200634d732a7b5fb156af7
Chiara Argese <argese.chiara@gmail.com>
2018-11-22 09:49:54 +0000

    new syntax, add urls

--- smn_oahpa_project/smn_oahpa/urls.py
+++ smn_oahpa_project/smn_oahpa/urls.py
@@ -1,4 +1,4 @@
-"""smn_oahpa URL Configuration
+"""LLL1_oahpa URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -16,6 +16,28 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.static import serve
+from django.conf.urls import i18n
+
+from settings import LLL1
+import importlib
+oahpa_module = importlib.import_module(LLL1+'_oahpa')
+sdv = importlib.import_module(LLL1+'_oahpa.drill.views')
+scv = importlib.import_module(LLL1+'_oahpa.conf.views')
+
+prefix = oahpa_module.settings.URL_PREFIX
+MEDIA_ROOT = oahpa_module.settings.MEDIA_ROOT
+
+admin_url = r'^%s/admin/' % prefix
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^%s/$' % prefix, sdv.index, name='index'),
+	url(r'^%s/i18n/' % prefix, include('django.conf.urls.i18n')),
+	url(r'^%s/' % prefix, include(LLL1+'_oahpa.drill.urls')),
+	url(r'^%s/courses/' % prefix, include(LLL1+'_oahpa.courses.urls')),
+    url(r'^%s/media/(?P<path>.*)$' % prefix, serve, {'document_root': MEDIA_ROOT}),
+	url(r'^%s/dialect/$' % prefix, scv.dialect),
+    url(admin_url, admin.site.urls),
 ]
