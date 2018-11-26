47c7ed85e98d0c9b2585bc936e38f89fff83484a
Chiara Argese <argese.chiara@gmail.com>
2018-11-26 14:59:06 +0000

    abstraction

--- sme_oahpa_project/sme_oahpa/diff/models.py
+++ sme_oahpa_project/sme_oahpa/diff/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-from univ_drill.models import Word
+
+from local_conf import LLL1
+import importlib
+oahpa_module = importlib.import_module(LLL1+'_oahpa')
+
+Word = oahpa_module.drill.models.Word
 
 class WordDiff(models.Model):
 	word = models.ForeignKey(Word, unique=True)
