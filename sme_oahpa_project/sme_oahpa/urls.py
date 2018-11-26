9c9cffc703fcacbbd09b0430b44c4c4fd2621f3f
Chiara Argese <argese.chiara@gmail.com>
2018-11-26 13:56:29 +0000

    add needed urls from old_urls

--- sme_oahpa_project/sme_oahpa/urls.py
+++ sme_oahpa_project/sme_oahpa/urls.py
@@ -13,9 +13,35 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
+from django.conf.urls import i18n
+
+from settings import LLL1
+import importlib
+oahpa_module = importlib.import_module(LLL1+'_oahpa')
+sdv = importlib.import_module(LLL1+'_oahpa.drill.views')
+scv = importlib.import_module(LLL1+'_oahpa.conf.views')
+
+prefix = oahpa_module.settings.URL_PREFIX
+MEDIA_ROOT = oahpa_module.settings.MEDIA_ROOT
+
+admin_url = r'^%s/admin/' % prefix
 
 urlpatterns = [
+    url(r'^davvi/$', sdv.index),
+    url(r'^%s/i18n/' % prefix, include('django.conf.urls.i18n')),
+    url(r'^davvi/', include(LLL1+'_oahpa.drill.urls')),
+    url(r'^davvi/courses/', include(LLL1+'_oahpa.courses.urls')),
+    url(r'^%s/survey/' % prefix, include(LLL1+'_oahpa.survey.urls')),
+    url(r'^%s/errorapi/' % prefix, include(LLL1+'_oahpa.errorapi.urls')),
+    url(r'^%s/messages/' % prefix, include('django_messages.urls')),
+    url(r'^%s/accounts/login/$' % prefix, 'courses.auth_views.cookie_login'),
+    url(r'^%s/media/(?P<path>.*)$' % prefix, serve, {'document_root': MEDIA_ROOT}),
+    url(r'^%s/dialect/$' % prefix, scv.dialect),
+    url(r'^%s/favicon\.ico$' % prefix,
+        'django.views.generic.simple.redirect_to',
+        {'url': '/%s/media/images/favicon_16x16.ico' % prefix}),
     url(r'^admin/', admin.site.urls),
 ]
