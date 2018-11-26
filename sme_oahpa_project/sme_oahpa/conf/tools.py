a58ff2f3ea6694eb8167cdcfd72a2e914ccbf943
Chiara Argese <argese.chiara@gmail.com>
2018-11-26 14:11:48 +0000

    abstraction

--- sme_oahpa_project/sme_oahpa/conf/tools.py
+++ sme_oahpa_project/sme_oahpa/conf/tools.py
@@ -1,18 +1,18 @@
-from univ_oahpa.settings import OLD_NEW_ISO_CODES as ISO, LANGUAGE_CODE as SCODE
+from local_conf import LLL1
+import importlib
+oahpa_module = importlib.import_module(LLL1+'_oahpa')
 
 def switch_language_code(CODE):
 	"""
 		Switches language codes from ISO 639-1 to ISO 639-2.
-		
+
 		>>> switch_language_code("no")
 		"nob"
-		
+
 	"""
 	try:
-		return ISO[CODE]
+		return oahpa_module.settings.OLD_NEW_ISO_CODES[CODE]
 	except:
 		# logger.warning("*** Unidentified language code %s." % CODE)
 		# print >> sys.stdout, "*** Unidentified language code %s." % CODE
-		return SCODE
-	
-
+		return oahpa_module.settings.LANGUAGE_CODE
