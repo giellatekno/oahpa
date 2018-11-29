a1952e878df1e92d5239f93661b66bdb9a4c68de
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2018-11-29 19:46:00 +0000

    added the first version feed_db script to be compared to the tentative_feed_db one

---
+++ sme_oahpa_project/feed_db.sh
@@ -0,0 +1,148 @@
+#!/bin/sh
+
+# run the script:
+# sh feed_db.sh sme (= LLL1)
+
+P="python"
+LLL1=$1
+log_file="db_install_error.log"
+DATA="${LLL1}_data"
+META="$DATA/meta_data"
+SRC="$DATA/src"
+XXX="$DATA/*2${LLL1}"
+
+rm -fv $log_file
+
+echo "==================================================="
+echo "fixing collation to utf-8"
+cat fix_collation.sql | $P manage.py dbshell
+echo "==================================================="
+echo " "
+echo "done"
+echo "==================================================="
+
+
+echo "==================================================="
+echo "installing tags and paradigms for Morfa-C"
+$P install.py -r $META/paradigms.txt -t $META/tags.txt -b 2>>error.log
+echo " "
+echo "done"
+echo "==================================================="
+
+
+echo "==================================================="
+echo "installing grammar links for norwegian"
+$P install.py -i $META/grammatikklinker.txt 2>>error.log
+echo " "
+echo "done"
+echo "==================================================="
+
+##
+##  sme->X
+##
+
+for xfile in $(ls $SRC/*.xml)
+do
+    fl=$(basename $xfile)
+    # the first substring before the first '_' in the xml file name is the POS ('prop' for both pers and geo)
+    POS=${fl%%_*}
+    PARA_FILE="${META}/${POS}_paradigms.txt"
+
+    echo "feeding db with: $xfile and para_file $PARA_FILE and pos $POS"
+
+    if [ "$xfile" != "" or ]; then
+
+	if [ -e "$PARA_FILE" ]; then
+	    echo "File exists $PARA_FILE"
+
+	    $P install.py --file $xfile --tagfile $META/tags.txt --paradigmfile $PARA_FILE 2>>$log_file
+
+	else
+	    echo "File does not exist $PARA_FILE"
+
+	    $P install.py --file $xfile 2>>$log_file
+
+	fi
+	echo " "
+	echo "done"
+    fi
+done
+
+
+
+=======
+=======
+=======
+=======
+=======
+=======
+=======
+=======
+
+echo "==================================================="
+echo "installing tags and paradigms for Morfa-C"
+$P install.py -r $META/paradigms.txt -t $META/tags.txt -b 2>>$log_file
+echo " "
+echo "done"
+echo "==================================================="
+
+# NB:
+#sms_oahpa>export DJANGO_SETTINGS_MODULE=sms_oahpa.settings
+
+for xfile in $(ls $SRC/*.xml)
+do
+    fl=$(basename $xfile)
+    POS=${fl%_*}
+    PARA_FILE="${META}/${POS}_paradigms.txt"
+
+
+    echo "feeding db with: $xfile"
+
+
+    if [ -e "$PARA_FILE" ]; then
+	echo "File exists $PARA_FILE"
+
+	$P install.py --file $xfile --tagfile $META/tags.txt --paradigmfile $PARA_FILE 2>>$log_file
+
+    else
+	echo "File does not exist $PARA_FILE"
+
+	$P install.py --file $xfile 2>>$log_file
+
+    fi
+    echo " "
+    echo "done"
+done
+
+for xfile in $(ls $XXX/*.xml)
+do
+  echo "feeding db with: $xfile"
+  $P install.py -f $xfile 2>>$log_file
+  echo " "
+  echo "done"
+  echo "   "
+done
+
+echo "==================================================="
+echo "feeding db with $META/semantic_sets.xml"
+$P install.py --sem $META/semantic_sets.xml 2>>$log_file
+echo " "
+echo "done"
+echo "==================================================="
+
+
+
+$P manage.py mergetags
+$P manage.py fixtagattributes
+
+
+
+echo "==================================================="
+echo "installing Morfa-C questions for nouns"
+$P install.py -g $META/grammar_defaults.xml -q $META/noun_questions.xml 2>>$log_file
+echo " "
+echo "done"
+echo "==================================================="
+
+echo "stopped at: "
+date '+%T'
