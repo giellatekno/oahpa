1d786ebf0afe9819fd656f584365cad54bb34a6f
Chiara Argese <argese.chiara@gmail.com>
2018-12-03 15:21:21 +0000

    new syntax

--- sme_oahpa_project/sme_oahpa/errorapi/urls.py
+++ sme_oahpa_project/sme_oahpa/errorapi/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
-urlpatterns = patterns('errorapi.views',
-    url(r'^lookup/$', 'error_feedback_view'),
-    url(r'^test_page/$', 'test_page'),
-)
+from local_conf import LLL1
+import importlib
+oahpa_module = importlib.import_module(LLL1+'_oahpa')
+sev = importlib.import_module(LLL1+'_oahpa.errorapi.views')
+
+urlpatterns = [
+    url(r'^lookup/$', sev.error_feedback_view),
+    url(r'^test_page/$', sev.test_page),
+]
 
 # vim: set ts=4 sw=4 tw=72 syntax=python :
