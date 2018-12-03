7a33831576e46e8eda06d57b816d61ae5ec8ffc5
Chiara Argese <argese.chiara@gmail.com>
2018-12-03 15:55:07 +0000

    new syntax and comment messages, still to be fixed

--- sme_oahpa_project/sme_oahpa/urls.py
+++ sme_oahpa_project/sme_oahpa/urls.py
@@ -23,6 +23,9 @@ import importlib
 oahpa_module = importlib.import_module(LLL1+'_oahpa')
 sdv = importlib.import_module(LLL1+'_oahpa.drill.views')
 scv = importlib.import_module(LLL1+'_oahpa.conf.views')
+sav = importlib.import_module(LLL1+'_oahpa.courses.auth_views')
+
+from django.views.generic import RedirectView
 
 prefix = oahpa_module.settings.URL_PREFIX
 MEDIA_ROOT = oahpa_module.settings.MEDIA_ROOT
@@ -36,12 +39,13 @@ urlpatterns = [
     url(r'^davvi/courses/', include(LLL1+'_oahpa.courses.urls')),
     url(r'^%s/survey/' % prefix, include(LLL1+'_oahpa.survey.urls')),
     url(r'^%s/errorapi/' % prefix, include(LLL1+'_oahpa.errorapi.urls')),
-    url(r'^%s/messages/' % prefix, include('django_messages.urls')),
-    url(r'^%s/accounts/login/$' % prefix, 'courses.auth_views.cookie_login'),
+    #url(r'^%s/messages/' % prefix, include('django_messages.urls')),
+    url(r'^%s/accounts/login/$' % prefix, sav.cookie_login),
     url(r'^%s/media/(?P<path>.*)$' % prefix, serve, {'document_root': MEDIA_ROOT}),
     url(r'^%s/dialect/$' % prefix, scv.dialect),
-    url(r'^%s/favicon\.ico$' % prefix,
-        'django.views.generic.simple.redirect_to',
-        {'url': '/%s/media/images/favicon_16x16.ico' % prefix}),
+    #url(r'^%s/favicon\.ico$' % prefix,
+    #    'django.views.generic.simple.redirect_to',
+    #    {'url': '/%s/media/images/favicon_16x16.ico' % prefix}),
+    url(r'^%s/favicon\.ico$' % prefix, RedirectView.as_view(url='/%s/media/images/favicon_16x16.ico')),
     url(r'^admin/', admin.site.urls),
 ]
