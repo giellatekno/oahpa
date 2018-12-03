e681f8ad8a2500f6a94e6b29c1f2a84e97d71321
Chiara Argese <argese.chiara@gmail.com>
2018-12-03 15:17:03 +0000

    abstraction and new syntax

--- sme_oahpa_project/sme_oahpa/survey/urls.py
+++ sme_oahpa_project/sme_oahpa/survey/urls.py
@@ -1,16 +1,21 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 from rest_framework import routers
 
 from views import SurveyView, AnswerView
 from views import answer, dismiss
 
+from local_conf import LLL1
+import importlib
+oahpa_module = importlib.import_module(LLL1+'_oahpa')
+ssv = importlib.import_module(LLL1+'_oahpa.survey.views')
+
 router = routers.DefaultRouter()
 router.register(r'surveys', SurveyView, base_name='surveys')
 router.register(r'answer', AnswerView, base_name='answer')
 
-urlpatterns = patterns('survey.views',
-    url(r'^dismiss/$', 'dismiss'),
-    url(r'^answer/$', 'answer', name="answer_survey"),
+urlpatterns = [
+    url(r'^dismiss/$', ssv.dismiss),
+    url(r'^answer/$', ssv.answer, name="answer_survey"),
     url(r'^api/', include(router.urls)),
-)
+]
