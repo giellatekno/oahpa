bc8acedaa8b6c4e3efad0f5fa45ce1b0bad60155
Chiara Argese <argese.chiara@gmail.com>
2019-01-25 08:33:31 +0000

    abstraction

--- crk_oahpa_project/crk_oahpa/wsgi.py
+++ crk_oahpa_project/crk_oahpa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crk_oahpa project.
+WSGI config for LLL1_OAHPA project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crk_oahpa.settings")
+from settings import LLL1
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", LLL1+"_oahpa.settings")
 
 application = get_wsgi_application()
