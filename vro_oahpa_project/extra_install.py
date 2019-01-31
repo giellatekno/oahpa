1e928a2e25ab903cc30ba4bd6e745de62e01e178
Chiara Argese <argese.chiara@gmail.com>
2019-01-31 10:34:50 +0000

    abstraction in extra_install

--- vro_oahpa_project/extra_install.py
+++ vro_oahpa_project/extra_install.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from settings import *
-from vro_drill.models import *
+# -*- coding: utf-8 -*-
+from local_conf import LLL1
+import importlib
+settings = importlib.import_module(LLL1+'_oahpa.settings')
+sdm = importlib.import_module(LLL1+'_oahpa.drill.models')
+
 from django.db.models import Q
 from xml.dom import minidom as _dom
 from django.utils.encoding import force_unicode
@@ -15,14 +19,14 @@ languages = [
     'vro',
     'nob',
 	'eng',
-	'dan', 
+	'dan',
 	'swe',
 ]
 
 class Link(object):
 	def get_lang(self):
 		""" Assumes that the file language is stored as part of the
-			file name in the link, e.g., 
+			file name in the link, e.g.,
 
 			substantiv.nob.html
 					   ^
@@ -36,45 +40,45 @@ class Link(object):
 			language = 'sme'
 		if language == 'nno':
 			language = 'nob' # this is probably not needed here.
-		
+
 		self.language = language
 
 	def __init__(self, S):
 		S = S.strip()
 		self.S = S
-		
+
 		keyword, _, link = S.partition('\t')
 		self.keyword = keyword
 		self.url = link
 
 		self.get_lang()
-	
+
 	def create_obj(self):
 		kwargs = {'name': self.keyword,
 					'address': self.url,
 					'language': self.language}
 
-		self.obj, _  = Grammarlinks.objects.get_or_create(**kwargs)
+		self.obj, _  = sdm.Grammarlinks.objects.get_or_create(**kwargs)
 
 
-	
+
 
 
 class Extra:
 
 	# Installs links to the grammatical information under giellatekno.
 	# The link list appears to the upper right corner of the oahpa-pages.
-	# The links are in the file sme/meta/grammarlinks.txt 
+	# The links are in the file sme/meta/grammarlinks.txt
 	def read_address(self,linkfile):
 
-		
+
 		linkfileObj = open(linkfile, "r")
 		data = [l for l in linkfileObj.readlines() if l.strip()]
-		
+
 		links = [Link(l) for l in data]
 		languages = list(set([link.language for link in links]))
 
-		linkobjects = Grammarlinks.objects.filter(language__in=languages).delete()
+		linkobjects = sdm.Grammarlinks.objects.filter(language__in=languages).delete()
 
 		for link in links:
 			try:
@@ -87,16 +91,16 @@ class Extra:
 
 
 
-				
+
 	#The comments presented to the user after completing the game.
 	def read_comments(self, commentfile):
 		xmlfile=file(commentfile)
-		tree = _dom.parse(commentfile)		
+		tree = _dom.parse(commentfile)
 
 		comments_el = tree.getElementsByTagName("comments")[0]
 		lang = comments_el.getAttribute("xml:lang")
 
-		comments = Comment.objects.filter(lang=lang)
+		comments = sdm.Comment.objects.filter(lang=lang)
 		for c in comments:
 			c.delete()
 		for el in comments_el.getElementsByTagName("comment"):
@@ -104,7 +108,7 @@ class Extra:
 			for com in el.getElementsByTagName("text"):
 				text = com.firstChild.data
 				print text
-				comment, created = Comment.objects.get_or_create(lang=lang, comment=text, level=level)
+				comment, created = sdm.Comment.objects.get_or_create(lang=lang, comment=text, level=level)
 				comment.save()
 
 	# Installs the semantic superclasses
@@ -117,17 +121,15 @@ class Extra:
 		for el in tree.getElementsByTagName("subclasses"):
 			semclass=el.getAttribute("class")
 			print semclass
-			s, created = Semtype.objects.get_or_create(semtype=semclass)
+			s, created = sdm.Semtype.objects.get_or_create(semtype=semclass)
 			for el2 in el.getElementsByTagName('sem'):
 			   subclass  = el2.getAttribute("class")
 			   print "\t" + subclass
-			   for w in Word.objects.filter(Q(semtype__semtype=subclass) & ~Q(semtype__semtype=semclass)):
+			   for w in sdm.Word.objects.filter(Q(semtype__semtype=subclass) & ~Q(semtype__semtype=semclass)):
 				   w.semtype.add(s)
 				   print u"\t%s added to word id: %d" % (s, w.id)
 				   w.save()
-			   for w in WordTranslation.objects.filter(Q(semtype__semtype=subclass) & ~Q(semtype__semtype=semclass)):
+			   for w in sdm.WordTranslation.objects.filter(Q(semtype__semtype=subclass) & ~Q(semtype__semtype=semclass)):
 				   w.semtype.add(s)
 				   print u"\t%s added to %d" % (s, w.id)
 				   w.save()
-
-
