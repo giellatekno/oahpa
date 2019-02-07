f7b897582f2597feac51334a8f5dc6c6e6881b68
Chiara Argese <argese.chiara@gmail.com>
2019-02-07 15:06:53 +0000

    add context_processors and middleware

--- vro_oahpa_project/vro_oahpa/settings.py
+++ vro_oahpa_project/vro_oahpa/settings.py
@@ -53,7 +53,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.admindocs.middleware.XViewMiddleware'
+    'django.contrib.admindocs.middleware.XViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = LLL1+'_oahpa.urls'
@@ -70,6 +72,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.static',
             ],
         },
     },
