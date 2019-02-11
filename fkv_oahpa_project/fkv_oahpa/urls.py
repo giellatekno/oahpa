5e01d852d03405c66719839e25f975d1e24cd04a
Chiara Argese <argese.chiara@gmail.com>
2019-02-11 15:07:48 +0000

    fix urls

--- fkv_oahpa_project/fkv_oahpa/urls.py
+++ fkv_oahpa_project/fkv_oahpa/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
+
+from settings import LLL1
+import importlib
+oahpa_module = importlib.import_module(LLL1+'_oahpa')
+sdv = importlib.import_module(LLL1+'_oahpa.drill.views')
+scv = importlib.import_module(LLL1+'_oahpa.conf.views')
+
+from settings import URL_PREFIX as prefix
+MEDIA_ROOT = oahpa_module.settings.MEDIA_ROOT
 
 urlpatterns = [
+    url(r'^%s/$' % prefix, sdv.index),
+    url(r'^%s/i18n/' % prefix, include('django.conf.urls.i18n')),
+    url(r'^%s/' % prefix, include(LLL1+'_oahpa.drill.urls')),
+    url(r'^%s/courses/' % prefix, include(LLL1+'_oahpa.courses.urls')),
+    url(r'^%s/media/(?P<path>.*)$' % prefix, serve, {'document_root': MEDIA_ROOT}),
+    url(r'^%s/dialect/$' % prefix, scv.dialect),
     url(r'^admin/', admin.site.urls),
 ]
