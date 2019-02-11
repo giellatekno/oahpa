b828ee34821214e292627b38274a4b364699462c
Chiara Argese <argese.chiara@gmail.com>
2019-02-11 12:17:24 +0000

    merge settings new and old

--- fkv_oahpa_project/fkv_oahpa/settings.py
+++ fkv_oahpa_project/fkv_oahpa/settings.py
@@ -15,22 +15,27 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import sys
+os.environ['PYTHON_EGG_CACHE'] = '/tmp'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'fkv_oahpa.settings'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+MAIN_LANGUAGE = ('fkv', 'Kven')
+LLL1 = MAIN_LANGUAGE[0]
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pp^l$gk61x4g5afb5tc6=#0lp8u2so^$4@d#_u4_ndj!0se!#m'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEV = True
 
-ALLOWED_HOSTS = []
+INTERNAL_IPS = ('127.0.0.1',)
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    LLL1+'_oahpa.fkv_drill',
+    LLL1+'_oahpa.conf',
+    LLL1+'_oahpa.courses',
+    LLL1+'_oahpa.fkv_feedback',
+    LLL1+'_oahpa.management',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,14 +52,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
-ROOT_URLCONF = 'fkv_oahpa.urls'
+ROOT_URLCONF = LLL1+'_oahpa.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+    	        os.path.join(os.path.dirname(__file__), 'drill/templates').replace('\\','/'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,23 +69,17 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+                LLL1+"_oahpa.courses.context_processors.request_user",
+				LLL1+"_oahpa.conf.context_processors.dialect",
+				LLL1+"_oahpa.conf.context_processors.site_root",
+				LLL1+"_oahpa.conf.context_processors.grammarlinks",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'fkv_oahpa.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+WSGI_APPLICATION = LLL1+'_oahpa.wsgi.application'
 
 
 # Password validation
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'no'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Oslo'
 
 USE_I18N = True
 
@@ -113,8 +114,66 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+URL_PREFIX = 'kveeni'
+
+MEDIA_URL = '/%s/media/' % URL_PREFIX
+
+LANGUAGES = (
+    ('fkv', 'Kven'),
+    ('ru', 'Russian'),
+    ('sme', 'North Sami'),
+	('no', 'Norwegian'),
+	#('sv', 'Swedish'),
+	('en', 'English'),
+	('fi', 'Finnish'),
+	#('de', 'German'),
+)
+
+OLD_NEW_ISO_CODES = {
+	"fi": "fin",
+	"ru": "rus",
+	"en": "eng",
+	"no": "nob",
+	"de": "deu",
+	"sv": "swe",
+	"sma": "sma",
+    "sme": "sme",
+    "fkv": "fkv"
+}
+
+
+INFINITIVE_SUBTRACT = {
+	'nob': ur'^(?P<inf>å )?(?P<lemma>.*)$',
+	'swe': ur'^(?P<inf>att )?(?P<lemma>.*)$',
+	'eng': ur'^(?P<inf>to )?(?P<lemma>.*)$',
+	'deu': ur'^(?P<inf>zu )?(?P<lemma>.*)$',
+}
+
+INFINITIVE_ADD = {
+	'nob': ur'å \g<lemma>',
+	'swe': ur'att \g<lemma>',
+	'eng': ur'to \g<lemma>',
+	'deu': ur'zu \g<lemma>',
+}
+
+DIALECTS = {
+	'main': ('generator-oahpa-gt-norm.xfst', 'Unrestricted'),
+#	'GG': ('isme-GG.restr.fst', 'Western'),
+#	'KJ': ('isme-KJ.restr.fst', 'Eastern'),
+	'NG': (None, 'Non-Presented forms'),
+}
+
+DEFAULT_DIALECT = 'main'
+NONGEN_DIALECT = 'NG'
+# # #
+#
+# Some settings for the install.py scripts
+#
+# # #
+
+from settings_not_in_svn import *
