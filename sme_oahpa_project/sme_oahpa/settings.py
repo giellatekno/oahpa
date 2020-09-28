21543a5eb9cbfc8b0d0867ff06640e7298aed96b
Chiara Argese <argese.chiara@gmail.com>
2020-09-28 12:03:06 +0000

    change language_code to no since there is locale/no but not locale/nb

--- sme_oahpa_project/sme_oahpa/settings.py
+++ sme_oahpa_project/sme_oahpa/settings.py
@@ -145,7 +145,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'nb'
+LANGUAGE_CODE = 'no'
 
 TIME_ZONE = 'Europe/Oslo'
 
